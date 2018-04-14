601959c1fd5564d5fdafea75686b80801a215d15
sterraf <sterraf@users.noreply.github.com>
2018-04-14 13:32:23 -0300

    pongo otras propiedades de forces(). completo un poco más los locales de MA y enuncio Rasiowa-Sikorski.

--- forces_locale.thy
+++ forces_locale.thy
@@ -23,14 +23,44 @@ definition (in forcing_poset)
   "compat(p,q) == compat_in(p,q,P)"
 
 definition (in forcing_poset)
+  antichain :: "i\<Rightarrow>o" where
+  "antichain(A) == A\<subseteq>P \<and> (\<forall>p\<in>A.\<forall>q\<in>A.(\<not>compat(p,q)))"
+
+definition (in forcing_poset)
   filter :: "i\<Rightarrow>o" where
   "filter(G) == G\<subseteq>P \<and> increasing(G) \<and> (\<forall>p\<in>G. \<forall>q\<in>G. compat_in(p,q,G))"
 
+definition (in forcing_poset) 
+  up_closure :: "i\<Rightarrow>i" where
+  "up_closure(A) == {p\<in>P.\<exists>a\<in>A.<a,p>\<in>leq}"
+
+lemma (in forcing_poset) closure_compat_filter:
+  "A\<subseteq>P \<Longrightarrow> (\<forall>p\<in>A.\<forall>q\<in>A. compat(p,q)) \<Longrightarrow> filter(up_closure(A))"
+oops
+    
+lemma  (in forcing_poset) chain_compat:
+  "A\<subseteq>P \<Longrightarrow> linear(A,leq) \<Longrightarrow>  (\<forall>p\<in>A.\<forall>q\<in>A. compat(p,q))"
+oops      
+  
+locale countable_generic = forcing_poset +
+  fixes \<D>
+  assumes countable_subs_of_P:  "\<D> \<in> nat\<rightarrow>\<P>(P)"
+  and     seq_of_denses:        "dense(\<D>`n)"
+
+definition (in countable_generic)
+  D_generic :: "i\<Rightarrow>o" where
+  "D_generic(G) == filter(G) \<and> (\<forall>n\<in>nat.(\<D>`n)\<inter>G\<noteq>0)"
+
+ 
+theorem (in countable_generic) rasiowa_sikorski: 
+  "p\<in>P \<Longrightarrow> \<exists>G. p\<in>G \<and> D_generic(G)"
+oops
+  
 locale forcing_data = forcing_poset +
   fixes M enum
   assumes trans_M:          "Transset(M)"
       and M_model_ZF:       "satT(M,ZFTh,[])"
-      and M_countable:      "enum\<in>bij(M,\<omega>)"
+      and M_countable:      "enum\<in>bij(nat,M)"
       and P_in_M:           "P \<in> M"
 
 definition (in forcing_data)
@@ -40,10 +70,13 @@ definition (in forcing_data)
 (* Prototyping Forcing relation and theorems as a locale*)
 locale forcing_thms = forcing_poset + forcing_data +
   fixes forces :: "i \<Rightarrow> i"
-  assumes forces_type:      "forces(\<phi>) \<in> formula"
-      and truth_lemma:      "\<forall>env\<in>list(M).
+  assumes definition_of_forces: "\<forall>env\<in>list(M).
                   sats(M,forces(\<phi>), [P,leq,uno,p] @ env) \<longleftrightarrow>
-                  (\<forall>G.(G\<subseteq>P \<and> generic(G)\<and> p\<in>G)\<longrightarrow>sats(gen_ext(M,P,G),\<phi>,map(valR(M,P,G),env)))"
+                  (\<forall>G.(generic(G)\<and> p\<in>G)\<longrightarrow>sats(gen_ext(M,P,G),\<phi>,map(valR(M,P,G),env)))"
+      and definability:      "forces(\<phi>) \<in> formula"
+      and truth_lemma:      "\<forall>env\<in>list(M).\<forall>G.(generic(G) \<and> p\<in>G)\<longrightarrow>
+                  ((\<exists>p\<in>P.(sats(M,forces(\<phi>), [P,leq,uno,p] @ env))) \<longleftrightarrow>
+                  (sats(gen_ext(M,P,G),\<phi>,map(valR(M,P,G),env))))"
 
 lemma (in forcing_thms) bounded_separation_in_genext:
     "\<forall>p\<in>formula. arity(p)<5 \<longrightarrow> sats(gen_ext(M,P,G),ZFSeparation(p),[])"
