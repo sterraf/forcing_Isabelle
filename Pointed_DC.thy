1913f7e05ef38b8469b4eadc51f18efb8fff27bd
sterraf <sterraf@users.noreply.github.com>
2018-05-05 10:22:11 -0300

    estaba lleno de bugs

--- Pointed_DC.thy
+++ Pointed_DC.thy
@@ -1,12 +1,15 @@
 theory Pointed_DC imports AC
+
 begin
+txt\<open>This proof of DC is from Moschovakis "Notes on Set Theory"\<close>
+
 consts dc_witness :: "i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> i"
 primrec
   wit0   : "dc_witness(0,A,a,s,R) = a"
   witrec :"dc_witness(succ(n),A,a,s,R) = s`{x\<in>A. <dc_witness(n,A,a,s,R),x>\<in>R }"
   
 lemma witness_into_A [TC]:  "a\<in>A \<Longrightarrow> n\<in>nat \<Longrightarrow>
-                             (\<forall>X . X\<noteq>0 \<and> X\<subseteq>A \<longrightarrow> s`X\<in>A) \<Longrightarrow>
+                             (\<forall>X . X\<noteq>0 \<and> X\<subseteq>A \<longrightarrow> s`X\<in>X) \<Longrightarrow>
                              \<forall>y\<in>A. {x\<in>A. <y,x>\<in>R } \<noteq> 0 \<Longrightarrow>
                              dc_witness(n, A, a, s, R)\<in>A"
   apply (induct_tac n ,simp+)
@@ -14,9 +17,18 @@ lemma witness_into_A [TC]:  "a\<in>A \<Longrightarrow> n\<in>nat \<Longrightarro
   apply (drule_tac x="{xa \<in> A . \<langle>dc_witness(x, A, a, s, R), xa\<rangle> \<in> R}" in  spec)
   apply auto 
   done
-
+lemma witness_related :  "a\<in>A \<Longrightarrow> n\<in>nat \<Longrightarrow>
+                             (\<forall>X . X\<noteq>0 \<and> X\<subseteq>A \<longrightarrow> s`X\<in>X) \<Longrightarrow>
+                             \<forall>y\<in>A. {x\<in>A. <y,x>\<in>R } \<noteq> 0 \<Longrightarrow>
+                             <dc_witness(n, A, a, s, R),dc_witness(succ(n), A, a, s, R)>\<in>R"
+  apply (frule_tac n="n" and s="s"  and R="R" in witness_into_A, assumption+)
+   apply (drule_tac x="dc_witness(n, A, a, s, R)" in bspec, assumption)
+  apply (drule_tac x= "{x \<in> A . \<langle>dc_witness(n, A, a, s, R), x\<rangle> \<in> R}" in spec)
+  apply (simp, blast) 
+  done
+    
 lemma witness_funtype: "a\<in>A \<Longrightarrow> 
-                        (\<forall>X . X\<noteq>0 \<and> X\<subseteq>A \<longrightarrow> s`X\<in>A) \<Longrightarrow>
+                        (\<forall>X . X\<noteq>0 \<and> X\<subseteq>A \<longrightarrow> s`X\<in>X) \<Longrightarrow>
                         \<forall>y\<in>A. {x\<in>A. <y,x>\<in>R } \<noteq> 0 \<Longrightarrow>
                         (\<lambda>n\<in>nat. dc_witness(n, A, a, s, R)) \<in> nat -> A"
   apply (rule_tac B="{dc_witness(n, A, a, s, R). n\<in>nat}" in fun_weaken_type)
@@ -24,14 +36,14 @@ lemma witness_funtype: "a\<in>A \<Longrightarrow>
   apply ( blast intro:witness_into_A)
   done
     
-lemma witness_to_fun: "a\<in>A \<Longrightarrow> (\<forall>X . X\<noteq>0 \<and> X\<subseteq>A \<longrightarrow> s`X\<in>A) \<Longrightarrow>
+lemma witness_to_fun: "a\<in>A \<Longrightarrow> (\<forall>X . X\<noteq>0 \<and> X\<subseteq>A \<longrightarrow> s`X\<in>X) \<Longrightarrow>
                              \<forall>y\<in>A. {x\<in>A. <y,x>\<in>R } \<noteq> 0 \<Longrightarrow>
                              \<exists>f \<in> nat->A. \<forall>n\<in>nat. f`n =dc_witness(n,A,a,s,R)"
   apply (rule_tac x="\<lambda>n\<in>nat. dc_witness(n,A,a,s,R)" in  bexI, simp)
   apply (rule witness_funtype, simp+)
   done
 
-lemma pointed_DC  : "\<forall>R.  (\<forall>x\<in>A. \<exists>y\<in>A. <x,y>\<in> R) \<Longrightarrow>
+theorem pointed_DC  : "(\<forall>x\<in>A. \<exists>y\<in>A. <x,y>\<in> R) \<Longrightarrow>
                        \<forall>a\<in>A. (\<exists>f \<in> nat->A. f`0 = a \<and> (\<forall>n \<in> nat. <f`n,f`succ(n)>\<in>R))"
   apply (rule)
   apply (insert AC_func_Pow)
@@ -44,7 +56,11 @@ lemma pointed_DC  : "\<forall>R.  (\<forall>x\<in>A. \<exists>y\<in>A. <x,y>\<in
   prefer 2 apply (assumption)           
   apply (rename_tac s)
   apply (rule_tac x="\<lambda>n\<in>nat. dc_witness(n,A,a,s,R)" in bexI)
-  apply auto
+   prefer 2  apply (blast intro:witness_funtype)
+  apply (rule  conjI, simp)
+  apply (rule ballI, rename_tac m)
+  apply (subst beta, simp)+
+  apply (rule witness_related, simp+, blast)
   done
     
 end
