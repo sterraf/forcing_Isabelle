5276d8291261f53f936f61c558e3e04013d606ac
sterraf <sterraf@users.noreply.github.com>
2018-05-18 14:40:39 -0300

    elimino (in LOCALE) usando bloques

--- src/Separation_generic.thy
+++ src/Separation_generic.thy
@@ -1,4 +1,4 @@
-theory Separation_generic imports  forcing_posets begin
+theory Separation_generic imports   Formula Names ZFCAxioms_formula forcing_posets begin
 
 locale forcing_data = forcing_poset +
   fixes M enum
@@ -9,11 +9,12 @@ locale forcing_data = forcing_poset +
       (* TODO: Quitar estas assumptions cuando tengamos el Relative hacked *)
       and M_repl:           "\<And>P. replacement(##M,P)"
       and M_nat:            "nat \<in> M"
-  
-definition (in forcing_data)
+begin  
+definition
   generic :: "i\<Rightarrow>o" where
   "generic(G) == filter(G) \<and> (\<forall>D\<in>M. D\<subseteq>P \<and> dense(D)\<longrightarrow>D\<inter>G\<noteq>0)"
-      
+  
+end      
 (* Prototyping Forcing relation and theorems as a locale*)
 locale forcing_thms = forcing_poset + forcing_data +
   fixes forces :: "i \<Rightarrow> i"
@@ -24,8 +25,8 @@ locale forcing_thms = forcing_poset + forcing_data +
       and truth_lemma:      "\<forall>env\<in>list(M).\<forall>G.(generic(G) \<and> p\<in>G)\<longrightarrow>
                   ((\<exists>p\<in>P.(sats(M,forces(\<phi>), [P,leq,uno,p] @ env))) \<longleftrightarrow>
                   (sats(gen_ext(M,P,G),\<phi>,map(valR(M,P,G),env))))"
-
-lemma (in forcing_thms)
+begin
+lemma
   "\<phi>\<in>formula \<Longrightarrow> \<psi>\<in>formula \<Longrightarrow> 
     \<forall>u\<in>M. \<forall>l\<in>M. \<forall>Q\<in>M. \<forall>s\<in>M. \<forall>r\<in>M. \<forall>d\<in>M.
       sats(M,[d,r,s,Q,l,u],
@@ -33,14 +34,14 @@ lemma (in forcing_thms)
           forces(And(Member(0,1),\<phi>))))))" 
   oops
    
-lemma (in forcing_thms)
+lemma
     "sats(M,forces(And(Member(0,1),\<phi>)),[P,leq,uno,p,\<theta>,\<pi>,\<sigma>])
       \<Longrightarrow> valR(M,P,G,{w\<in>domain(\<pi>)\<times>P. x=x}) =x"  (* Enunciado mal *)
   oops
     
       
-lemma (in forcing_thms) bounded_separation_in_genext:
-    "\<forall>p\<in>formula. arity(p) = 3 \<longrightarrow> sats(gen_ext(M,P,G),ZFSeparation(p),[])"
+lemma bounded_separation_in_genext:
+    "\<forall>p\<in>formula. arity(p) = 3 \<longrightarrow> sats(gen_ext(M,P,G),Separation_ax_fm(p),[])"
 oops
-  
+end  
 end
