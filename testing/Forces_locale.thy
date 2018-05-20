84beb83f52474fae914aa320b7145f9738204c2c
sterraf <sterraf@users.noreply.github.com>
2018-05-20 20:34:39 -0300

    mejoro enunciado de reforzarmiento de condiciones

--- testing/Forces_locale.thy
+++ testing/Forces_locale.thy
@@ -12,26 +12,24 @@ end
 (* Prototyping Forcing relation and theorems as a locale*)
 locale forcing_thms = forcing_data +
   fixes forces :: "i \<Rightarrow> i"
-  assumes definition_of_forces: "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow>
-                \<forall>env\<in>list(M).
+  assumes definition_of_forces: "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow> env\<in>list(M) \<Longrightarrow>
                   sats(M,forces(\<phi>), [P,leq,uno,p] @ env) \<longleftrightarrow>
                   (\<forall>G.(generic(G)\<and> p\<in>G)\<longrightarrow>sats(gen_ext(M,P,G),\<phi>,map(valR(M,P,G),env)))"
       and  definability:     "\<phi>\<in>formula \<Longrightarrow> forces(\<phi>) \<in> formula"
-      and   truth_lemma:     "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow>
-                 \<forall>env\<in>list(M).\<forall>G.(generic(G) \<and> p\<in>G)\<longrightarrow>
+      and   truth_lemma:     "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow> env\<in>list(M) \<Longrightarrow>
+                 \<forall>G.(generic(G) \<and> p\<in>G)\<longrightarrow>
                   ((\<exists>p\<in>P.(sats(M,forces(\<phi>), [P,leq,uno,p] @ env))) \<longleftrightarrow>
                   (sats(gen_ext(M,P,G),\<phi>,map(valR(M,P,G),env))))"
-      and    cone_lemma:     "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow>
-                 \<forall>env\<in>list(M).
-                    sats(M,forces(\<phi>), [P,leq,uno,p] @ env) \<longleftrightarrow> 
-                    (\<forall>q\<in>P. <q,p>\<in>leq \<longrightarrow> sats(M,forces(\<phi>), [P,leq,uno,q] @ env))"
-      and density_lemma:     "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow>
+      and  streghtening:     "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow> env\<in>list(M) \<Longrightarrow> q\<in>P \<Longrightarrow> <q,p>\<in>leq \<Longrightarrow>
+                               sats(M,forces(\<phi>), [P,leq,uno,p] @ env) \<Longrightarrow>
+                               sats(M,forces(\<phi>), [P,leq,uno,q] @ env)"
+      and density_lemma:     "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow> env\<in>list(M) \<Longrightarrow>
                  \<forall>env\<in>list(M).
                     sats(M,forces(\<phi>), [P,leq,uno,p] @ env) \<longleftrightarrow> 
                     dense_below({q\<in>P. sats(M,forces(\<phi>), [P,leq,uno,q] @ env)},p)"
 
 begin  (*************** CONTEXT: forcing_thms *****************)
-
+  
 end
   
 end
