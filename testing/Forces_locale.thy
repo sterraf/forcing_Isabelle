d9640fb07ccdeec3160d79caa91acde9da744bbc
sterraf <sterraf@users.noreply.github.com>
2018-05-20 15:35:03 -0300

    relacion de forces con el orden de P

--- testing/Forces_locale.thy
+++ testing/Forces_locale.thy
@@ -1,28 +1,37 @@
-theory Forces_locale imports Interface forcing_posets begin
+theory Forces_locale imports Interface Forcing_data begin
 
-locale forcing_data = forcing_poset +
-  fixes M enum
-  assumes trans_M:          "Transset(M)"
-      and M_model_ZF:       "satT(M,ZFTh,[])"
-      and M_countable:      "enum\<in>bij(nat,M)"
-      and P_in_M:           "P \<in> M"
-      and zero_in_M:        "0\<in> M"
-
-
+context forcing_data
 begin
 lemma to_M_basic_no_repl :
   "M_basic_no_repl(##M)"
   by (insert trans_M M_model_ZF zero_in_M,rule interface_M_basic)
 
 interpretation M?: M_basic_no_repl "##M" by (rule to_M_basic_no_repl)
+end
+  
+(* Prototyping Forcing relation and theorems as a locale*)
+locale forcing_thms = forcing_data +
+  fixes forces :: "i \<Rightarrow> i"
+  assumes definition_of_forces: "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow>
+                \<forall>env\<in>list(M).
+                  sats(M,forces(\<phi>), [P,leq,uno,p] @ env) \<longleftrightarrow>
+                  (\<forall>G.(generic(G)\<and> p\<in>G)\<longrightarrow>sats(gen_ext(M,P,G),\<phi>,map(valR(M,P,G),env)))"
+      and  definability:     "\<phi>\<in>formula \<Longrightarrow> forces(\<phi>) \<in> formula"
+      and   truth_lemma:     "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow>
+                 \<forall>env\<in>list(M).\<forall>G.(generic(G) \<and> p\<in>G)\<longrightarrow>
+                  ((\<exists>p\<in>P.(sats(M,forces(\<phi>), [P,leq,uno,p] @ env))) \<longleftrightarrow>
+                  (sats(gen_ext(M,P,G),\<phi>,map(valR(M,P,G),env))))"
+      and    cone_lemma:     "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow>
+                 \<forall>env\<in>list(M).
+                    sats(M,forces(\<phi>), [P,leq,uno,p] @ env) \<longleftrightarrow> 
+                    (\<forall>q\<in>P. <q,p>\<in>leq \<longrightarrow> sats(M,forces(\<phi>), [P,leq,uno,q] @ env))"
+(*    and density_lemma:     "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow>
+                 \<forall>env\<in>list(M).
+                    sats(M,forces(\<phi>), [P,leq,uno,p] @ env) \<longleftrightarrow> 
+                    (\<forall>q\<in>P. <q,p>\<in>leq \<longrightarrow> sats(M,forces(\<phi>), [P,leq,uno,q] @ env))" *)
 
-lemma sep_instance_abs:
-  "\<lbrakk> \<phi> \<in> formula ; arity(\<phi>) = 7 ; a1\<in>M ; a2\<in>M ; a3\<in>M ; a4\<in>M ; a5\<in>M ;A\<in>M\<rbrakk> \<Longrightarrow> 
-    {x\<in>A. sats(M,\<phi>,[x,A,a1,a2,a3,a4,a5])} \<in> M"
-   apply (subgoal_tac "separation(##M,\<lambda>x. sats(M,\<phi>,[x,A,a1,a2,a3,a4,a5]))") 
-   apply (simp del:setclass_iff add:setclass_iff[symmetric])
-  sorry
+begin  (*************** CONTEXT: forcing_thms *****************)
 
 end
-
+  
 end
