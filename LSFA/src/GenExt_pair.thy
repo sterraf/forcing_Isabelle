7dc77a2c3d56a20564f44e90b32b78f0431891f7
Manu <manu.gunther@gmail.com>
2018-06-21 21:39:12 -0300

    escribiendo sobre preservación de pairing

--- LSFA/src/GenExt_pair.thy
+++ LSFA/src/GenExt_pair.thy
@@ -13,40 +13,31 @@ done
   
 lemma upairs_in_M :
   "upair_ax(##M) \<Longrightarrow> a \<in> M \<Longrightarrow> b \<in> M \<Longrightarrow> {a,b} \<in> M"
-  apply (rule_tac M="##M" and P="\<lambda>z. upair(##M,a,b,z)" in rexE)
-  apply (rule_tac M="##M" and x="b" and P="\<lambda>y. \<exists>z[##M]. upair(##M,a,y,z)" in rspec)
-  apply (rule_tac M="##M" and x="a" and 
-            P="\<lambda>x. \<forall>y[##M]. \<exists>z[##M]. upair(##M,x,y,z)" in rspec)
-  apply (simp add: upair_ax_def,simp+)
+  apply (simp add: upair_ax_def)
 done
 
 lemma one_in_M : "one \<in> M"
   by (insert trans_M,insert one_in_P,insert P_in_M,rule Transset_M)
  
 lemma pairs_in_M : 
-  " upair_ax(##M) \<Longrightarrow> a \<in> M \<Longrightarrow> b \<in> M \<Longrightarrow> {\<langle>a,one\<rangle>,\<langle>b,one\<rangle>} \<in> M"
-  apply (insert one_in_M)
+  " \<lbrakk> upair_ax(##M) ; a \<in> M ; b \<in> M ; c \<in> M ; d \<in> M \<rbrakk> \<Longrightarrow> {\<langle>a,c\<rangle>,\<langle>b,d\<rangle>} \<in> M"
   apply (unfold Pair_def)
   apply ((rule upairs_in_M)+,assumption+)+
 done
 
-lemma sigma_as_collect :
-  "{\<langle>\<tau>,one\<rangle>,\<langle>\<rho>,one\<rangle>} = {x\<in>{\<tau>,\<rho>}\<times>P. snd(x) = one} "
-  apply (insert one_in_P)
-  apply auto
-done
-    
+lemma sigma_in_M :
+  "upair_ax(##M) \<Longrightarrow> one \<in> G \<Longrightarrow> \<tau> \<in> M \<Longrightarrow> \<rho> \<in> M \<Longrightarrow> {\<langle>\<tau>,one\<rangle>,\<langle>\<rho>,one\<rangle>} \<in> M"
+  by (rule pairs_in_M,simp_all add: upair_ax_def one_in_M)
+  
 lemma valsigma :
   "one \<in> G \<Longrightarrow> \<tau> \<in> M \<Longrightarrow> \<rho> \<in> M \<Longrightarrow> {\<langle>\<tau>,one\<rangle>,\<langle>\<rho>,one\<rangle>} \<in> M \<Longrightarrow>
    val(G,{\<langle>\<tau>,one\<rangle>,\<langle>\<rho>,one\<rangle>}) = {val(G,\<tau>),val(G,\<rho>)}"
   apply (insert one_in_P)
-  apply (subst sigma_as_collect)
   apply (rule trans)
-  apply (rule val_of_name)
-  apply (subst sigma_as_collect[symmetric],assumption)
-  apply auto
+   apply (rule def_val,assumption)
+  apply (auto simp add: Sep_and_Replace)
 done
-    
+      
 lemma pair_preserv : 
   "one \<in> G \<Longrightarrow> upair_ax(##M) \<Longrightarrow> upair_ax(##M[G])"
   apply (simp add: upair_ax_def)
@@ -57,15 +48,11 @@ lemma pair_preserv :
   apply (rename_tac x y \<tau> \<rho>)
   apply (rule_tac x="val(G,{\<langle>\<tau>,one\<rangle>,\<langle>\<rho>,one\<rangle>})" in bexI)
    apply (subst valsigma,assumption+)
-  apply (rule pairs_in_M,simp_all add: upair_ax_def)
-  apply (rule_tac b="x" in ssubst,assumption)
-   apply (rule_tac b="y" in ssubst,assumption)
-   apply (simp add: upair_def)
-  apply (rule def_GenExt2)
-  apply (rule pairs_in_M,simp_all add: upair_ax_def)
+   defer 1 apply (simp add: upair_def)
+   apply (rule def_GenExt2)
+   apply (insert sigma_in_M,simp_all add: upair_ax_def)
 done
-  
-  
+
 end
 end
   
