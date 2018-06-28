dca2eae4e6b5a92707ac2f57fff10b9b4381a6d9
sterraf <sterraf@users.noreply.github.com>
2018-06-28 17:15:56 -0300

    version muy rota

--- src/Separation_generic.thy
+++ src/Separation_generic.thy
@@ -4,7 +4,17 @@ context forcing_thms begin
 
 lemma val_mono : "x\<subseteq>y \<Longrightarrow> val(G,x) \<subseteq> val(G,y)"
   sorry
+
+definition 
+  perm_sep_forces :: "i" where
+  "perm_sep_forces == {\<langle>0, 4\<rangle>, \<langle>1, 3\<rangle>, \<langle>2, 7\<rangle>, \<langle>3, 0\<rangle>, \<langle>4, 1\<rangle>, \<langle>5, 2\<rangle>, \<langle>6, 5\<rangle>, \<langle>7, 6\<rangle>}"
+
+lemma perm_sep_bij: "perm_sep_forces \<in> bij(8,8)" 
+  sorry
     
+lemma perm_sep_env : "perm_list(perm_sep_forces,[p,q,r,s,t,u,v,w]) = [t,s,w,p,q,r,u,v]"
+  sorry
+  
 lemmas transitivity = Transset_intf trans_M
 
 lemma iff_impl_trans: "Q\<longleftrightarrow>R \<Longrightarrow> R\<longrightarrow>S \<Longrightarrow> Q \<longrightarrow>S"
@@ -25,10 +35,10 @@ proof -
   then consider (1) ?P | (2) ?Q ..
   then show ?thesis
   proof cases
-    case 1
+    case (1)
     then show ?thesis sorry
   next
-    case 2
+    case (2)
     fix G c w 
     assume
          asm: "c\<in>M[G]" "w\<in>M[G]" "\<phi>\<in>formula" "M_generic(G)"
@@ -59,17 +69,37 @@ proof -
         assume 
          Eq4: "u \<in> domain(\<pi>) \<times> P"
         with Eq1 and Eq2 and Eq3 have
-                "sats(M,?\<psi>,[u] @ ?env @ [\<pi>,\<sigma>]) \<longleftrightarrow> 
-                  (\<exists>\<theta>\<in>M. \<exists>p\<in>M. u =<\<theta>,p> \<and> sats(M,forces(?\<chi>),[\<theta>,p,u]@?env@[\<pi>,\<sigma>]))"
+              "sats(M,?\<psi>,[u] @ ?env @ [\<pi>,\<sigma>]) \<longleftrightarrow> 
+                        (\<exists>\<theta>\<in>M. \<exists>p\<in>M. u =<\<theta>,p> \<and> 
+                          sats(M,forces(?\<chi>),[\<theta>,p,u]@?env@[\<pi>,\<sigma>]))"
           by (auto simp add: transitivity)
         also have
-                " ... \<longleftrightarrow> 
-                  (\<exists>\<theta>\<in>M. \<exists>p\<in>P. u =<\<theta>,p> \<and> sats(M,forces(?\<chi>),[\<theta>,p,u]@?env@[\<pi>,\<sigma>]))"
-          using Eq4 P_in_M by (auto simp add:transitivity)
-        also have
               " ... \<longleftrightarrow> (\<exists>\<theta>\<in>M. \<exists>p\<in>P. u =<\<theta>,p> \<and> 
-                          sats(M,forces(?\<chi>),?env@[p,\<theta>,\<pi>,\<sigma>]))"
-          using Eq4 sorry
+                          sats(M,rename(forces(?\<chi>))`length(?env@[p,\<theta>,\<pi>,\<sigma>,u])`converse(perm_sep_forces),[\<theta>,p,u]@?env@[\<pi>,\<sigma>]))"
+          (* using Eq4 P_in_M by (auto simp add:transitivity)*) sorry
+         also have
+              " ... \<longleftrightarrow> (\<exists>\<theta>\<in>M. \<exists>p\<in>P. u =<\<theta>,p> \<and> 
+                          sats(M,rename(forces(?\<chi>))`length(?env@[p,\<theta>,\<pi>,\<sigma>,u])`converse(perm_sep_forces),
+                          perm_list(perm_sep_forces,?env@[p,\<theta>,\<pi>,\<sigma>,u])))"
+           by (simp add: perm_sep_env)
+         also have
+              " ... \<longleftrightarrow> (\<exists>\<theta>\<in>M. \<exists>p\<in>P. u =<\<theta>,p> \<and> 
+                          sats(M,forces(?\<chi>),?env@[p,\<theta>,\<pi>,\<sigma>,u]))"
+         proof 
+           from Eq2 trans_M have
+         Eq5: "u \<in> M"
+             by (auto simp add:transD)
+               
+           have
+              "\<And>\<theta> p u. \<theta>\<in>M \<Longrightarrow> p\<in>P \<Longrightarrow> u \<in> M \<Longrightarrow> sats(M,rename(forces(?\<chi>))`length(?env@[p,\<theta>,\<pi>,\<sigma>,u])`converse(perm_sep_forces),
+                          perm_list(perm_sep_forces,?env@[p,\<theta>,\<pi>,\<sigma>,u]))
+                \<longleftrightarrow>
+                     sats(M,forces(?\<chi>),?env@[p,\<theta>,\<pi>,\<sigma>,u])"
+             using asm P_in_M leq_in_M uno_in_P Eq1  2 transD trans_M
+             apply(rule_tac ren_Sat_leq [symmetric])
+                apply(auto simp add: perm_sep_bij arity_forces nat_union_abs1)
+               
+               
         also have
               " ... \<longleftrightarrow> (\<exists>\<theta>\<in>M. \<exists>p\<in>P. u =<\<theta>,p> \<and> 
                           (\<forall>F. M_generic(F) \<and> p \<in> F \<longrightarrow> 
