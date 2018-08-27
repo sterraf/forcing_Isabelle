ab010ade0d0487d08c9a25bf0c1420fbaf521d30
sterraf <sterraf@users.noreply.github.com>
2018-08-27 20:32:28 -0300

    uno -> one. Manuuuu....?

--- src/Forces_locale.thy
+++ src/Forces_locale.thy
@@ -29,20 +29,20 @@ lemma [trans] : "x=y \<Longrightarrow> y\<subseteq>z \<Longrightarrow> x\<subset
 locale forcing_thms = forcing_data +
   fixes forces :: "i \<Rightarrow> i"
   assumes definition_of_forces: "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow> env\<in>list(M) \<Longrightarrow>
-                  sats(M,forces(\<phi>), [P,leq,uno,p] @ env) \<longleftrightarrow>
+                  sats(M,forces(\<phi>), [P,leq,one,p] @ env) \<longleftrightarrow>
                   (\<forall>G.(M_generic(G)\<and> p\<in>G)\<longrightarrow>sats(M[G],\<phi>,map(val(G),env)))"
       and  definability[TC]: "\<phi>\<in>formula \<Longrightarrow> forces(\<phi>) \<in> formula"
       and  arity_forces:     "\<phi>\<in>formula \<Longrightarrow> arity(forces(\<phi>)) = arity(\<phi>) #+ 4"
       and   truth_lemma:     "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow> env\<in>list(M) \<Longrightarrow>
                  \<forall>G.(M_generic(G) \<and> p\<in>G)\<longrightarrow>
-                  ((\<exists>p\<in>P.(sats(M,forces(\<phi>), [P,leq,uno,p] @ env))) \<longleftrightarrow>
+                  ((\<exists>p\<in>P.(sats(M,forces(\<phi>), [P,leq,one,p] @ env))) \<longleftrightarrow>
                   (sats(M[G],\<phi>,map(val(G),env))))"
       and  streghtening:     "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow> env\<in>list(M) \<Longrightarrow> q\<in>P \<Longrightarrow> <q,p>\<in>leq \<Longrightarrow>
-                               sats(M,forces(\<phi>), [P,leq,uno,p] @ env) \<Longrightarrow>
-                               sats(M,forces(\<phi>), [P,leq,uno,q] @ env)"
+                               sats(M,forces(\<phi>), [P,leq,one,p] @ env) \<Longrightarrow>
+                               sats(M,forces(\<phi>), [P,leq,one,q] @ env)"
       and density_lemma:     "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow> env\<in>list(M) \<Longrightarrow>
-                    sats(M,forces(\<phi>), [P,leq,uno,p] @ env) \<longleftrightarrow> 
-                    dense_below({q\<in>P. sats(M,forces(\<phi>), [P,leq,uno,q] @ env)},p)"
+                    sats(M,forces(\<phi>), [P,leq,one,p] @ env) \<longleftrightarrow> 
+                    dense_below({q\<in>P. sats(M,forces(\<phi>), [P,leq,one,q] @ env)},p)"
 
 begin  (*************** CONTEXT: forcing_thms *****************)
   
