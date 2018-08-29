e4e998449207bed0061d97f3e9a09b66906b54c0
sterraf <sterraf@users.noreply.github.com>
2018-08-28 21:23:21 -0300

    Correcciones a truth lemma

--- src/Forces_locale.thy
+++ src/Forces_locale.thy
@@ -33,10 +33,9 @@ locale forcing_thms = forcing_data +
                   (\<forall>G.(M_generic(G)\<and> p\<in>G)\<longrightarrow>sats(M[G],\<phi>,map(val(G),env)))"
       and  definability[TC]: "\<phi>\<in>formula \<Longrightarrow> forces(\<phi>) \<in> formula"
       and  arity_forces:     "\<phi>\<in>formula \<Longrightarrow> arity(forces(\<phi>)) = arity(\<phi>) #+ 4"
-      and   truth_lemma:     "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow> env\<in>list(M) \<Longrightarrow>
-                 \<forall>G.(M_generic(G) \<and> p\<in>G)\<longrightarrow>
-                  ((\<exists>p\<in>P.(sats(M,forces(\<phi>), [P,leq,one,p] @ env))) \<longleftrightarrow>
-                  (sats(M[G],\<phi>,map(val(G),env))))"
+      and   truth_lemma:     "\<phi>\<in>formula \<Longrightarrow> env\<in>list(M) \<Longrightarrow> M_generic(G) \<Longrightarrow>
+                  (\<exists>p\<in>G.(sats(M,forces(\<phi>), [P,leq,one,p] @ env))) \<longleftrightarrow>
+                  sats(M[G],\<phi>,map(val(G),env))"
       and  streghtening:     "p\<in>P \<Longrightarrow> \<phi>\<in>formula \<Longrightarrow> env\<in>list(M) \<Longrightarrow> q\<in>P \<Longrightarrow> <q,p>\<in>leq \<Longrightarrow>
                                sats(M,forces(\<phi>), [P,leq,one,p] @ env) \<Longrightarrow>
                                sats(M,forces(\<phi>), [P,leq,one,q] @ env)"
