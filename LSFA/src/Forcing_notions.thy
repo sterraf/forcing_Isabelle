00c3c8ea09fdb5fc861df93c7e6894d3d8535a87
sterraf <sterraf@users.noreply.github.com>
2018-11-17 15:22:21 -0300

    cambio estilo de prueba de P_dense

--- LSFA/src/Forcing_notions.thy
+++ LSFA/src/Forcing_notions.thy
@@ -50,7 +50,9 @@ definition
   "dense_below(D,q) == \<forall>p\<in>P. \<langle>p,q\<rangle>\<in>leq \<longrightarrow> (\<exists>d\<in>D . \<langle>d,p\<rangle>\<in>leq)"
 
 lemma P_dense: "dense(P)"
-  by (insert leq_preord, auto simp add: preorder_on_def refl_def dense_def)
+  using leq_preord
+  unfolding preorder_on_def refl_def dense_def
+  by blast  
     
 definition 
   increasing :: "i\<Rightarrow>o" where
