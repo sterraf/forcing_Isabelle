4922495ac4bd512ce3b4b1ed3e6c131837d74b02
Miguel Pagano <miguel.pagano@gmail.com>
2018-12-20 02:16:13 -0300

    Pairing escrito casi completo en Isar

--- src/Pairing_Axiom.thy
+++ src/Pairing_Axiom.thy
@@ -3,47 +3,38 @@ theory Pairing_Axiom imports Names Interface begin
 context forcing_data
 begin
 
-lemma upairs_in_M :
-  "a \<in> M \<Longrightarrow> b \<in> M \<Longrightarrow> {a,b} \<in> M"
-  apply (insert upair_ax,simp add: upair_ax_def)
-done
-
-lemma one_in_M : "one \<in> M"
-  by (insert trans_M,insert one_in_P,insert P_in_M,rule Transset_M)
- 
-lemma pairs_in_M : 
-  " \<lbrakk> a \<in> M ; b \<in> M ; c \<in> M ; d \<in> M \<rbrakk> \<Longrightarrow> {\<langle>a,c\<rangle>,\<langle>b,d\<rangle>} \<in> M"
-  by (insert upair_ax,unfold Pair_def,((rule upairs_in_M)+,assumption+)+)
-
-lemma sigma_in_M :
-  "one \<in> G \<Longrightarrow> \<tau> \<in> M \<Longrightarrow> \<rho> \<in> M \<Longrightarrow> {\<langle>\<tau>,one\<rangle>,\<langle>\<rho>,one\<rangle>} \<in> M"
-  by (insert upair_ax,rule pairs_in_M,simp_all add: upair_ax_def one_in_M)
-  
 lemma valsigma :
   "one \<in> G \<Longrightarrow> {\<langle>\<tau>,one\<rangle>,\<langle>\<rho>,one\<rangle>} \<in> M \<Longrightarrow>
    val(G,{\<langle>\<tau>,one\<rangle>,\<langle>\<rho>,one\<rangle>}) = {val(G,\<tau>),val(G,\<rho>)}"
-  apply (insert one_in_P)
-  apply (rule trans)
-   apply (rule def_val)
-  apply (auto simp add: Sep_and_Replace)
-done
+  by (insert one_in_P, rule trans, subst def_val,auto simp add: Sep_and_Replace)
       
-(* de la siguiente assumption solo usamos one \<in> G *)
 lemma pairing_in_MG : 
-  "M_generic(G) \<Longrightarrow> upair_ax(##M[G])"
-  apply (insert upair_ax)
-  apply (simp add: upair_ax_def)
-  apply (rule ballI)+
-  apply (drule GenExtD)+
-  apply (rule bexE,assumption)
-  apply (rule_tac A="M" and P="\<lambda>w. y=val(G,w)" in bexE,assumption)
-  apply (rename_tac x y \<tau> \<rho>)
-  apply (rule_tac x="val(G,{\<langle>\<tau>,one\<rangle>,\<langle>\<rho>,one\<rangle>})" in bexI)
-   apply (subst valsigma,rule one_in_G,assumption)
-    defer 1 apply (simp add: upair_def)
-   apply (rule GenExtI)
-   apply (insert sigma_in_M,simp_all add: upair_ax_def)
-done
+  assumes "M_generic(G)"
+  shows "upair_ax(##M[G])"
+proof - 
+  {
+    fix x y
+    have "one\<in>G" using assms one_in_G by simp
+    from assms have "G\<subseteq>P" 
+      unfolding M_generic_def and filter_def by simp
+    with \<open>one\<in>G\<close>have "one\<in>P" using subsetD by simp
+    then have "one\<in>M" using Transset_intf[OF trans_M _ P_in_M] by simp
+    assume "x \<in> M[G]" "y \<in> M[G]"
+    then obtain \<tau> \<rho> where
+      0 : "val(G,\<tau>) = x" "val(G,\<rho>) = y" "\<rho> \<in> M"  "\<tau> \<in> M"
+      using GenExtD by blast
+    with \<open>one\<in>M\<close> have "\<langle>\<tau>,one\<rangle> \<in> M" "\<langle>\<rho>,one\<rangle>\<in>M" 
+      using pair_in_M_iff by auto
+    then have 1: "{\<langle>\<tau>,one\<rangle>,\<langle>\<rho>,one\<rangle>} \<in> M" (is "?\<sigma> \<in> _")
+      using upair_in_M_iff by simp
+    then have "val(G,?\<sigma>) \<in> M[G]"
+      using GenExtI by simp
+    with 1 have "{val(G,\<tau>),val(G,\<rho>)} \<in> M[G]" 
+      using valsigma assms one_in_G by simp
+    with 0 have "{x,y} \<in> M[G]" by simp
+    }
+    then show ?thesis unfolding upair_ax_def upair_def by auto
+qed
 
 end
 end
