c19be68492a9862108fb4068b2c2549aec814f31
sterraf <sterraf@users.noreply.github.com>
2019-01-12 23:29:26 -0300

    sacando código al pedo. Dejo comentado el SHA posta

--- LICS/hacer_diff.sh
+++ LICS/hacer_diff.sh
@@ -3,7 +3,10 @@ DIFF_DIR=diff_dir
 PREFIX=LICS
 OLD_DIR=$DIFF_DIR/$PREFIX"_old"
 NEW_DIR=$DIFF_DIR/$PREFIX"_new"
+## SHA for testing purposes
 OLDSHA=73ed3222f1edb01dd260f7ea7f9bfe4acb785fa5
+## SHA of submitted version (the real thing)
+# OLDSHA=457792cabb9db838fe26a23a9b70d735423a45c0
 MAIN=Separation_In_MG
 
 function clear_appendix {
@@ -19,22 +22,11 @@ git archive $OLDSHA | tar -x -C $OLD_DIR
 cp * $NEW_DIR
 clear_appendix $OLD_DIR/$MAIN.tex
 clear_appendix $NEW_DIR/$MAIN.tex
-# sed -i -- 's@\\appendix\[A short overview of our development\]@@g' $OLD_DIR/$MAIN.tex
-# sed -i -- 's@\\appendix\[A short overview of our development\]@@g' $NEW_DIR/$MAIN.tex
-# sed -i -- 's@\\input{appendix}@@g' $OLD_DIR/$MAIN.tex
-# sed -i -- 's@\\input{appendix}@@g' $NEW_DIR/$MAIN.tex
-# sed -i -- 's@\\newpage\n\\onecolumn@@g' $OLD_DIR/$MAIN.tex
-# sed -i -- 's@\\newpage\n\\onecolumn@@g' $NEW_DIR/$MAIN.tex
 cd $DIFF_DIR
 latexdiff --flatten $PREFIX"_old"/$MAIN.tex $PREFIX"_new"/$MAIN.tex > $PREFIX"_new"/diff.tex
 cd $PREFIX"_new"/
 sed -i -- 's/Local Variables://g' diff.tex
-sed -i -- 's/bibliographystyle{entcs}/bibliographystyle{mi-estilo-else}/g' diff.tex
 sed -i -- 's@bibliography{../LSFA/citados}@bibliography{../../../LSFA/citados}@g' diff.tex
-# cp ../../mi-estilo-else.bst .
-# cp diff.tex diff.tmp
-# cat ../../header-of-diff.tex > diff.tex
-# tail -n +67 diff.tmp >> diff.tex
 pdflatex \\nonstopmode\\input diff.tex
 pdflatex \\nonstopmode\\input diff.tex
 bibtex diff.aux
