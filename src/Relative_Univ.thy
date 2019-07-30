845ecd9d5fe76c3cad2bae2c1fafd4f4651da2a9
sterraf <sterraf@users.noreply.github.com>
2019-07-30 11:19:31 -0300

    modificaciones beta

--- src/Relative_Univ.thy
+++ src/Relative_Univ.thy
@@ -3,11 +3,74 @@ theory Relative_Univ
     Datatype_absolute
 
 begin
+lemma (in M_trivial) powerset_subset_Pow:
+  assumes 
+    "powerset(M,x,y)" "\<And>z. z\<in>y \<Longrightarrow> M(z)"
+  shows 
+    "y \<subseteq> Pow(x)"
+  using assms unfolding powerset_def
+  by (auto)
+    
+lemma (in M_trivial) powerset_abs: 
+  assumes
+    "M(x)" "\<And>z. z\<in>y \<Longrightarrow> M(z)"
+  shows
+    "powerset(M,x,y) \<longleftrightarrow> y = {a\<in>Pow(x) . M(a)}"
+proof (intro iffI equalityI)
+  (* First show the converse implication by double inclusion *)
+  assume 
+    "powerset(M,x,y)"
+  with assms have
+    "y \<subseteq> Pow(x)" 
+    using powerset_subset_Pow by simp
+  with assms show
+    "y \<subseteq> {a\<in>Pow(x) . M(a)}"
+    by blast
+  {
+    fix a
+    assume 
+      "a \<subseteq> x" "M(a)"
+    then have 
+      "subset(M,a,x)" by simp
+    with \<open>M(a)\<close> \<open>powerset(M,x,y)\<close> have
+      "a \<in> y"
+      unfolding powerset_def by simp
+  }
+  then show 
+    "{a\<in>Pow(x) . M(a)} \<subseteq> y"
+    by auto
+next (* we show the direct implication *)
+  assume 
+    "y = {a \<in> Pow(x) . M(a)}"
+  then show
+    "powerset(M, x, y)"
+    unfolding powerset_def
+    by simp
+qed
+
+lemma (in M_trivial) powerset_abs' [simp]: 
+  assumes
+    "M(x)" "M(y)"
+  shows
+    "powerset(M,x,y) \<longleftrightarrow> y = {a\<in>Pow(x) . M(a)}"
+  using powerset_abs[OF _ transM] assms by simp
+
+lemma Collect_inter_Transset:
+  assumes 
+    "Transset(M)" "b \<in> M"
+  shows
+    "{x\<in>b . P(x)} = {x\<in>b . P(x)} \<inter> M"
+    using assms unfolding Transset_def
+  by (auto)  
 
 (* "Vfrom(A,i) == transrec(i, %x f. A \<union> (\<Union>y\<in>x. Pow(f`y)))" *)
 definition
   HVfrom :: "[i,i,i] \<Rightarrow> i" where
   "HVfrom(A,x,f) \<equiv> A \<union> (\<Union>y\<in>x. Pow(f`y))"
+
+definition
+  MHVfrom :: "[i\<Rightarrow>o,i,i,i] \<Rightarrow> i" where
+  "MHVfrom(M,A,x,f) \<equiv> A \<union> (\<Union>y\<in>x. {a\<in>Pow(f`y). M(a)})"
 
 (* z = Pow(f`y) *)
 definition
@@ -24,16 +87,29 @@ definition
   is_Vfrom :: "[i\<Rightarrow>o,i,i,i] \<Rightarrow> o" where
   "is_Vfrom(M,A,i,V) == is_transrec(M,is_HVfrom(M,A),i,V)"
 
+
 context M_basic
 begin
 
-lemma is_powapply_abs: "\<lbrakk>M(f); M(y); M(z) \<rbrakk> \<Longrightarrow> is_powapply(M,f,y,z) \<longleftrightarrow> powerset(M, f ` y, z)"
+lemma is_powapply_abs: "\<lbrakk>M(f); M(y); M(z) \<rbrakk> \<Longrightarrow> is_powapply(M,f,y,z) \<longleftrightarrow> z = {x\<in>Pow(f`y). M(x)}"
   unfolding is_powapply_def by simp
 
 lemma "\<lbrakk>M(A); M(x); M(f); M(h) \<rbrakk> \<Longrightarrow> 
-      is_HVfrom(M,A,x,f,h) \<longleftrightarrow> (\<exists>R[M]. h =A \<union> \<Union>R \<and> Ex(M) \<and> is_Replace(M, x, \<lambda>x y. powerset(M, f ` x, y), R))"
+      is_HVfrom(M,A,x,f,h) \<longleftrightarrow> 
+      (\<exists>R[M]. h = A \<union> \<Union>R \<and> is_Replace(M, x,\<lambda>x y. y = {x \<in> Pow(f ` x) . M(x)}, R))"
   using is_powapply_abs unfolding is_HVfrom_def by auto
 
-end
+lemma "relation2(M,is_HVfrom(M,A),MHVfrom(M,A))"
+  unfolding is_HVfrom_def MHVfrom_def relation2_def
+  oops
 
+end (* context M_basic *)
+
+context M_trancl
+begin
+
+lemma Vfrom_abs: "\<lbrakk>M(A); M(i); M(V) \<rbrakk> \<Longrightarrow> is_Vfrom(M,A,i,V) \<longleftrightarrow> V = {x\<in>Vfrom(A,i). M(x)}"
+  sorry
+
+end
 end
