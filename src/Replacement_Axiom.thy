6e9cc6cffc7b03dae166bca4970d723515818477
sterraf <sterraf@users.noreply.github.com>
2019-08-20 13:03:26 -0300

    saco sorry de TC

--- src/Replacement_Axiom.thy
+++ src/Replacement_Axiom.thy
@@ -142,6 +142,9 @@ definition
   body_fm :: "i\<Rightarrow>i" where
   "body_fm(\<phi>) \<equiv> renbody`body_fm'(\<phi>)"
 
+lemma body_fm_type [TC]: "\<phi>\<in>formula \<Longrightarrow>  body_fm(\<phi>)\<in>formula"
+  unfolding body_fm_def by simp
+
 lemma sats_body_fm:
   assumes
     "\<exists>t p. x=<t,p>" "[\<alpha>,x,m,P,leq,one,\<pi>] @ nenv \<in>list(M)"
@@ -250,8 +253,8 @@ proof -
   moreover from \<open>arity(_) \<le> 3 #+ length(nenv)\<close>
   have "arity(?f_fm) \<le> 6 #+ length(env)" (* or 8? *)
     unfolding body_fm_def using arity_forces arity_renrep sorry
-  moreover
-  have "?f_fm\<in>formula" sorry
+  moreover from \<open>\<phi>\<in>formula\<close>
+  have "?f_fm\<in>formula" by simp
   moreover
   note inM = P_in_M leq_in_M one_in_M \<open>nenv\<in>list(M)\<close> \<open>\<pi>\<in>M\<close>
   ultimately
