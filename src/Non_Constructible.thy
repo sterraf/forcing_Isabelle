c71018a82491f35248fd626680ea3e89c4820326
sterraf <sterraf@users.noreply.github.com>
2019-12-15 11:54:30 -0300

    M[G] es extensión propia

--- src/Non_Constructible.thy
+++ src/Non_Constructible.thy
@@ -1,10 +1,11 @@
 theory Non_Constructible
   imports
-    Forcing_Theorems 
+    Forcing_Thms 
+    (* it actually depends more basic stuff, plus a misplaced lemma in the former *)
 
 begin
 
-lemma (in forcing_notion) filter_imp_compat: "filter(G) \<Longrightarrow> p\<in>G \<Longrightarrow> q\<in>G \<Longrightarrow> compat(p,q)"
+lemma (in forcing_notion) filter_imp_compat: "filter(G) \<Longrightarrow> p\<in>G \<Longrightarrow> q\<in>G \<Longrightarrow> compat(p,q)"  \<comment> \<open>put somewhere else\<close>
   unfolding filter_def compat_in_def compat_def by blast
 
 definition
@@ -38,7 +39,7 @@ lemma preorder_on_chle: "preorder_on(list(2),chle)"
 lemma Nil_chle_max: "x\<in>list(2) \<Longrightarrow> \<langle>x,[]\<rangle> \<in> chle"
   by auto
 
-interpretation ch: "forcing_notion" "list(2)" "chle" "[]"
+interpretation ch: forcing_notion "list(2)" "chle" "[]"
   unfolding forcing_notion_def using preorder_on_chle Nil_chle_max by simp
 
 abbreviation Chle :: "[i, i] \<Rightarrow> o"  (infixl "\<preceq>" 50)
@@ -52,7 +53,8 @@ lemma incompatible_extensions:
   shows "(p @ [0]) \<bottom> (p @ [1])"
   sorry
 
-lemma assumes "ch.filter(G)" shows "ch.dense(list(2) - G)"
+lemma filter_complement_dense:
+  assumes "ch.filter(G)" shows "ch.dense(list(2) - G)"
 proof
   fix p
   assume "p\<in>list(2)"
@@ -76,5 +78,50 @@ proof
     show ?thesis using ch.leq_reflI unfolding Diff_def by auto
   qed
 qed
+
+context M_ctm
+begin
+
+lemma poset_in_M: "list(2)\<in>M" 
+  using list_closed transitivity[OF _ nat_in_M] by simp
+
+lemma chle_in_M: "chle \<in> M"
+  unfolding chle_def chlerel_def Rrel_def chleR_def
+  sorry
+
+end (* M_ctm *)
+
+sublocale M_ctm \<subseteq> forcing_data "list(2)" "chle" "[]"
+  using poset_in_M chle_in_M by (unfold_locales)
+
+context M_ctm
+begin
+
+lemma generic_not_in_M: assumes "M_generic(G)"  shows "G \<notin> M"
+proof
+  assume "G\<in>M"
+  then
+  have "list(2) - G \<in> M" 
+    using P_in_M Diff_closed by simp
+  moreover
+  have "\<not>(\<exists>q\<in>G. q \<in> list(2) - G)" "(list(2) - G) \<subseteq> list(2)"
+    unfolding Diff_def by auto
+  moreover
+  note assms
+  ultimately
+  show "False"
+    using filter_complement_dense[of G] M_generic_denseD[of G "list(2)-G"] 
+      M_generic_def by simp \<comment> \<open>need to put generic ==> filter in claset\<close>
+qed
+
+lemma G_subset_M: "M_generic(G) \<Longrightarrow> G \<subseteq> M" \<comment> \<open>put somewhere else\<close>
+  using transitivity[OF _ P_in_M] by auto
+
+(* For some reason, "M[G]" doesn't work here *)
+theorem proper_extension: assumes "M_generic(G)" shows "M \<noteq> GenExt(G)"
+  using assms G_in_Gen_Ext[of G] one_in_G[of G] generic_not_in_M G_subset_M
+  by force
+
+end (* M_ctm *)
 
 end
