23119f86d96668466256db8c8ef5885244410847
sterraf <sterraf@users.noreply.github.com>
2020-01-02 12:06:36 -0300

    Makefile y aledaños

--- src/fix_links.sh
+++ src/fix_links.sh
@@ -3,7 +3,7 @@ old_dir=$PWD
 html_dir="output/html/Unsorted/Forcing/"
 
 cd $html_dir
-sed -i -- 's/a href="..\/..\/ZF\/ZF\/Main.html/a href="https:\/\/www.cl.cam.ac.uk\/research\/hvg\/Isabelle\/dist\/library\/ZF\/ZF\/ZF.html/g' *.html
+sed -i -- 's/a href="..\/..\/ZF\/ZF\/ZF.html/a href="https:\/\/www.cl.cam.ac.uk\/research\/hvg\/Isabelle\/dist\/library\/ZF\/ZF\/ZF.html/g' *.html
 sed -i -- 's/a href="..\/..\/ZF\/ZF\//a href="https:\/\/www.cl.cam.ac.uk\/research\/hvg\/Isabelle\/dist\/library\/ZF\/ZF\//g' *.html
 sed -i -- 's/a href="..\/..\/ZF\/ZF-Constructible\//a href="https:\/\/www.cl.cam.ac.uk\/research\/hvg\/Isabelle\/dist\/library\/ZF\/ZF-Constructible\//g' *.html
 cd $old_dir
