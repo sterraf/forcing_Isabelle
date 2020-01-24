75a6734817f0645e2a0e9497b63f36268f557f7d
sterraf <sterraf@users.noreply.github.com>
2020-01-24 01:02:22 -0300

    make actualizado

--- src/fix_links.sh
+++ src/fix_links.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 old_dir=$PWD
-html_dir="output/html/Unsorted/Forcing/"
+html_dir="output/html/ZF/Forcing/"
 
 cd $html_dir
 sed -i -- 's/a href="..\/..\/ZF\/ZF\/ZF.html/a href="https:\/\/www.cl.cam.ac.uk\/research\/hvg\/Isabelle\/dist\/library\/ZF\/ZF\/ZF.html/g' *.html
