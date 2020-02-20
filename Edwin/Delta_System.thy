09a162c78f9750351875146a99becf27e2535756
sterraf <sterraf@users.noreply.github.com>
2020-02-20 19:18:41 -0300

    comienzo de prueba del Δ-System Lemma

--- Edwin/Delta_System.thy
+++ Edwin/Delta_System.thy
@@ -127,11 +127,11 @@ proof -
     by (rule_tac le_trans[OF _ cardinal_UN_le]) (auto intro:Ord_0_le)+
 qed
 
-lemma nat_le_aleph1: "nat<\<aleph>1"
+lemma nat_lt_aleph1: "nat<\<aleph>1"
   by (simp add: Aleph_def lt_csucc)
 
-lemma zero_le_aleph1: "0<\<aleph>1"
-  by (rule lt_trans[of _ "nat"], auto simp add: ltI nat_le_aleph1)
+lemma zero_lt_aleph1: "0<\<aleph>1"
+  by (rule lt_trans[of _ "nat"], auto simp add: ltI nat_lt_aleph1)
 
 lemma le_aleph1_nat: "Card(k) \<Longrightarrow> k<\<aleph>1 \<Longrightarrow> k \<le> nat"    
   by (simp add: Aleph_def  Card_lt_csucc_iff Card_nat)
@@ -156,7 +156,10 @@ proof -
     using InfCard_nat leqpoll_imp_cardinal_UN_le by simp
 qed
 
-lemma "f:\<aleph>1\<rightarrow>nat \<Longrightarrow> \<exists>n\<in>nat. \<aleph>1 \<le> |f-``{n}|"
+lemma Pi_vimage_subset : "f \<in> Pi(A,B) \<Longrightarrow> f-``C \<subseteq> A"
+  unfolding Pi_def by auto
+
+lemma aleph1_eq_cardinal_vimage: "f:\<aleph>1\<rightarrow>nat \<Longrightarrow> \<exists>n\<in>nat. |f-``{n}| = \<aleph>1"
 proof -
   assume "f:\<aleph>1\<rightarrow>nat"
   then
@@ -169,8 +172,12 @@ proof -
   have "domain(?G) \<lesssim> nat"
     using subset_imp_lepoll by simp
   have "function(?G)" by (simp add:function_lam)
-  have "\<aleph>1 \<subseteq> (\<Union>n\<in>range(f). f-``{n})"
-  proof
+  from \<open>f:\<aleph>1\<rightarrow>nat\<close>
+  have "n\<in>nat \<Longrightarrow> f-``{n} \<subseteq> \<aleph>1" for n
+    using Pi_vimage_subset by simp
+  with \<open>range(f) \<subseteq> nat\<close>
+  have "\<aleph>1 = (\<Union>n\<in>range(f). f-``{n})"
+  proof (intro equalityI, intro subsetI)
     fix x 
     assume "x \<in> \<aleph>1"
     with \<open>f:\<aleph>1\<rightarrow>nat\<close> \<open>function(f)\<close> \<open>domain(f) = \<aleph>1\<close>
@@ -178,41 +185,102 @@ proof -
       using function_apply_Pair vimage_iff apply_rangeI by simp_all
     then 
     show "x \<in> (\<Union>n\<in>range(f). f-``{n})" by auto
-  qed
-  then
-  have "\<aleph>1 \<lesssim> (\<Union>n\<in>range(f). f-``{n})"
-    using subset_imp_lepoll by simp 
-  then 
-  have "|\<aleph>1| \<le> |\<Union>n\<in>range(f). f-``{n}|" 
-    by (rule lepoll_imp_Card_le)
+  qed auto
   {
     assume "\<forall>n\<in>range(f). |f-``{n}| < \<aleph>1"
     then 
     have "\<forall>n\<in>domain(?G). |?G`n| < \<aleph>1" 
-      using zero_le_aleph1 by (auto)
+      using zero_lt_aleph1 by (auto)
     with \<open>function(?G)\<close> \<open>domain(?G) \<lesssim> nat\<close> 
     have "|\<Union>n\<in>domain(?G). ?G`n|\<le>nat"
       using cof_aleph1_aux by (blast del:lepollD)  (* force/auto won't do it here *)
     then 
     have "|\<Union>n\<in>range(f). f-``{n}|\<le>nat" by simp
-    with \<open>|\<aleph>1| \<le> _\<close> 
-    have "|\<aleph>1| \<le> nat" by (rule le_trans) 
+    with \<open>\<aleph>1 = _\<close>
+    have "|\<aleph>1| \<le> nat" by simp
     then 
     have "\<aleph>1 \<le> nat"
-      using Aleph_def InfCard_is_Card Card_cardinal_eq Card_csucc by simp
+      using Card_Aleph Card_cardinal_eq
+      by simp
     then 
     have "False"
-      using nat_le_aleph1 by (blast dest:lt_trans2)
+      using nat_lt_aleph1 by (blast dest:lt_trans2)
   }
-  then 
-  have "\<exists>n\<in>range(f). \<not>(|f -`` {n}| < \<aleph>1)"
-    by auto
   with \<open>range(f)\<subseteq>nat\<close> 
-  show ?thesis
+  obtain n where "n\<in>nat" "\<not>(|f -`` {n}| < \<aleph>1)"
+    by blast
+  moreover from this
+  have "\<aleph>1 \<le> |f-``{n}|"
     using not_lt_iff_le Card_is_Ord by auto
+  moreover
+  note \<open>n\<in>nat \<Longrightarrow> f-``{n} \<subseteq> \<aleph>1\<close>
+  ultimately
+  show ?thesis
+    using subset_imp_le_cardinal[THEN le_anti_sym, of _ "\<aleph>1"]
+      Card_Aleph Card_cardinal_eq by auto
 qed
 
-lemma "|X|=\<aleph>1 \<Longrightarrow> f:X\<rightarrow>nat \<Longrightarrow> \<exists>n\<in>nat. |f-``{n}|=\<aleph>1" 
+lemma eqpoll_aleph1_cardinal_vimage:
+  assumes "X \<approx> \<aleph>1" "f : X \<rightarrow> nat"
+  shows "\<exists>n\<in>nat. |f-``{n}| = \<aleph>1"
   oops
+
+definition
+  delta_system :: "i \<Rightarrow> o" where
+  "delta_system(D) \<equiv> \<exists>r. \<forall>A\<in>D. \<forall>B\<in>D. A \<noteq> B \<longrightarrow> A \<inter> B = r"
+
+lemma delta_systemI[intro]: 
+  assumes "\<forall>A\<in>D. \<forall>B\<in>D. A \<noteq> B \<longrightarrow> A \<inter> B = r"
+  shows "delta_system(D)"
+  using assms unfolding delta_system_def by simp
+
+lemma delta_systemD[dest!]:
+  "delta_system(D) \<Longrightarrow> \<exists>r. \<forall>A\<in>D. \<forall>B\<in>D. A \<noteq> B \<longrightarrow> A \<inter> B = r"
+  unfolding delta_system_def by simp
+
+lemma delta_system_aleph1:
+  assumes "\<forall>A\<in>F. Finite(A)" "F \<approx> \<aleph>1"
+  shows "\<exists>D. D \<subseteq> F \<and> delta_system(D)"
+proof -
+  obtain n G where "n\<in>nat" "G \<subseteq> F" "A\<in>F \<Longrightarrow> |A| = n" for A
+    sorry
+  moreover
+  note assms
+  ultimately
+  show ?thesis
+  proof (induct arbitrary:F)
+    case 0
+    then
+    show ?case by auto
+  next
+    case (succ n)
+    show ?case
+    proof (cases "\<exists>p. {A\<in>G . p \<in> A} \<approx> \<aleph>1")
+      case True
+      then
+      obtain p where "{A\<in>G . p \<in> A} \<approx> \<aleph>1" by blast
+      from succ(1,3-5)
+      have "\<forall>A\<in>G. p\<in>A \<longrightarrow> |A - {p}| = n"
+        using Finite_imp_succ_cardinal_Diff[of _ p] by force
+      with succ
+      obtain D where "D\<subseteq>{A-{p} . A\<in>G}" "delta_system(D)"
+        by auto
+      then
+      obtain r where "\<forall>A\<in>D. \<forall>B\<in>D. A \<noteq> B \<longrightarrow> A \<inter> B = r"
+        by blast
+      then
+      show ?thesis by auto
+    next
+      case False
+      moreover from \<open>G\<subseteq>F\<close> \<open>F \<approx> \<aleph>1\<close>
+      have "{A \<in> G . p \<in> A} \<lesssim> \<aleph>1" for p
+        by (blast intro:lepoll_eq_trans[OF subset_imp_lepoll])
+      ultimately
+      have "{A \<in> G . p \<in> A} \<prec> \<aleph>1" for p
+        unfolding lesspoll_def by simp
+      then show ?thesis sorry
+    qed
+  qed
+qed
 
 end
