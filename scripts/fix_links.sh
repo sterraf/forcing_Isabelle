5b0e5817654918e83adf34ee0c917197e0bbc2a9
sterraf <sterraf@users.noreply.github.com>
2020-03-03 09:20:58 -0300

    reemplazo para tener links en el micrositio

--- scripts/fix_links.sh
+++ scripts/fix_links.sh
@@ -6,4 +6,5 @@ cd $html_dir
 sed -i -- 's/a href="..\/ZF\/ZF.html/a href="https:\/\/www.cl.cam.ac.uk\/research\/hvg\/Isabelle\/dist\/library\/ZF\/ZF\/ZF.html/g' *.html
 sed -i -- 's/a href="..\/ZF\/ZF\//a href="https:\/\/www.cl.cam.ac.uk\/research\/hvg\/Isabelle\/dist\/library\/ZF\/ZF\//g' *.html
 sed -i -- 's/a href="..\/ZF\/ZF-Constructible\//a href="https:\/\/www.cl.cam.ac.uk\/research\/hvg\/Isabelle\/dist\/library\/ZF\/ZF-Constructible\//g' *.html
+sed -i -- 's|a href="../ZF-Constructible-Trans/|a href="https://cs.famaf.unc.edu.ar/~pedro/forcing/ZF-Constructible-Trans/|g' *.html
 cd $old_dir
