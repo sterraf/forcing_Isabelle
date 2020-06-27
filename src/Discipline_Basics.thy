f607c74ad1e8e03efb11302ee1b769f2955939f3
sterraf <sterraf@users.noreply.github.com>
2020-06-27 10:22:46 -0300

    (incomplete) discipline for Pi using HO variables in new locale

--- src/Discipline_Basics.thy
+++ src/Discipline_Basics.thy
@@ -5,7 +5,7 @@ theory Discipline_Basics
     "../Tools/Try0"
 begin
 
-lemma (in M_basic) extensionality_trans: \<comment> \<open>General preliminary result\<close>
+lemma (in M_trivial) extensionality_trans: \<comment> \<open>General preliminary result\<close>
   assumes
     "M(d)" "M(d')"
     "\<forall>x[M]. x\<in>d  \<longleftrightarrow> P(x)"
@@ -29,7 +29,7 @@ subsection\<open>Discipline for \<^term>\<open>function_space\<close>\<close>
 abbreviation
   "is_function_space \<equiv> is_funspace"
 
-context M_basic
+context M_trivial
 begin
 
 lemma is_function_space_uniqueness:
@@ -85,7 +85,7 @@ lemma def_function_space_rel: "M(x) \<Longrightarrow> M(y) \<Longrightarrow> x \
   using  least_abs function_space_rel_closed function_space_rel_iff
   unfolding is_function_space_def by fastforce
 *)
-end (* M_basic *)
+end (* M_trivial *)
 
 (*****************  end Discipline  ***********************)
 
@@ -181,7 +181,7 @@ lemma def_inj_rel:
   assumes "M(A)" "M(B)"
   shows "inj_rel(A,B) =
          {f \<in> A \<rightarrow>r B.  \<forall>w[M]. \<forall>x[M]. w\<in>A \<and> x\<in>A \<and> f`w = f`x \<longrightarrow> w=x}"
-         (is "_ = Collect(_,?P)")
+    (is "_ = Collect(_,?P)")
 proof -
   from assms
   have "inj_rel(A, B) \<subseteq> A \<rightarrow>r B"
@@ -248,7 +248,7 @@ proof (intro iffI)
   with assms
   show "is_Pow(M, r, d)"
     using is_Pow_uniqueness[of r] is_Pow_witness
-    theI[OF ex1I[of "\<lambda>d. M(d) \<and> is_Pow(M,r,d)"], OF _ is_Pow_uniqueness[of r]]
+      theI[OF ex1I[of "\<lambda>d. M(d) \<and> is_Pow(M,r,d)"], OF _ is_Pow_uniqueness[of r]]
     unfolding Pow_rel_def
     by auto
 next
@@ -286,159 +286,196 @@ end (* M_basic *)
 
 (******************  end Discipline  **********************)
 
-(**********************************************************)
-subsection\<open>Discipline for \<^term>\<open>Sigma\<close>\<close>
+text\<open>A "Discipline" for terms involving \<^term>\<open>Replace\<close> is yet to
+be established.\<close>
+  (*
+Sigma(A,B) == \<Union>x\<in>A. \<Union>y\<in>B(x). {\<langle>x,y\<rangle>}
+           == \<Union> {  (\<Union>y\<in>B(x). {\<langle>x,y\<rangle>})  . x\<in>A}
+           == \<Union> {  (\<Union>y\<in>B(x). {\<langle>x,y\<rangle>})  . x\<in>A}
+           == \<Union> {  ( \<Union> { {\<langle>x,y\<rangle>} . y\<in>B(x)} )  . x\<in>A}
+                     ----------------------
+                           Sigfun(x,B)
+*)
 
-definition (* FAKE *)
-  is_Sigma   :: "[i\<Rightarrow>o,i,i\<Rightarrow>i,i]\<Rightarrow>o"  where
-  "is_Sigma(M,A,B,I) \<equiv> I = Sigma(A,B)"
+definition \<comment> \<open>FIX THIS: not completely relational. Can it be?\<close>
+  Sigfun :: "[i,i\<Rightarrow>i]\<Rightarrow>i"  where
+  "Sigfun(x,B) \<equiv> \<Union>y\<in>B(x). {\<langle>x,y\<rangle>}"
 
-context M_basic
-begin
+lemma Sigma_Sigfun: "Sigma(A,B) = \<Union> {Sigfun(x,B) . x\<in>A}"
+  unfolding Sigma_def Sigfun_def ..
 
-lemma is_Sigma_uniqueness:
+definition \<comment> \<open>FIX THIS: not completely relational. Can it be?\<close>
+  is_Sigfun :: "[i\<Rightarrow>o,i,i\<Rightarrow>i,i]\<Rightarrow>o"  where
+  "is_Sigfun(M,x,B,Sd) \<equiv> \<exists>RB[M]. is_Replace(M,B(x),\<lambda>y z. z={\<langle>x,y\<rangle>},RB) 
+                         \<and> big_union(M,RB,Sd)"
+
+context M_trivial
+begin 
+
+lemma is_Sigfun_abs:
   assumes
-    "M(r)" "\<And>i. M(i) \<Longrightarrow> M(y(i))" "M(d)" "M(d')"
-    "is_Sigma(M,r,y,d)" "is_Sigma(M,r,y,d')"
+    "strong_replacement(M,\<lambda>y z. z={\<langle>x,y\<rangle>})" 
+    "M(x)" "M(B(x))" "M(Sd)"
   shows
-    "d=d'"
-  using assms extensionality_trans
-  unfolding is_Sigma_def by simp
-
-lemma is_Sigma_witness: "M(r) \<Longrightarrow> (\<And>i. M(i) \<Longrightarrow> M(B(i))) \<Longrightarrow> \<exists>d[M]. is_Sigma(M,r,B,d)"
-  unfolding is_Sigma_def
-  sorry \<comment> \<open>We have to do this by hand, assuming axiom instance for \<^term>\<open>M\<close>\<close>
-
-definition
-  Sigma_rel :: "i \<Rightarrow> (i\<Rightarrow>i) \<Rightarrow> i"  where
-  "Sigma_rel(A,B) \<equiv> THE d. M(d) \<and> is_Sigma(M,A,B,d)"
-
-\<comment> \<open>From this point on, the higher order variable \<^term>\<open>y\<close> must be
-explicitly instantiated, and proof methods are slower\<close>
-lemma Sigma_rel_closed:
-  "M(x) \<Longrightarrow> (\<And>i. M(i) \<Longrightarrow> M(y(i))) \<Longrightarrow> M(Sigma_rel(x,y))"
-  unfolding Sigma_rel_def
-  using theI[OF ex1I[of "\<lambda>d. M(d) \<and> is_Sigma(M,x,y,d)"], OF _ is_Sigma_uniqueness[of x y]]
-    is_Sigma_witness[of _ y] by fastforce
-
-lemma Sigma_rel_iff:
-  assumes "M(x)" "(\<And>i. M(i) \<Longrightarrow> M(y(i)))" "M(d)"
-  shows "is_Sigma(M,x,y,d) \<longleftrightarrow> d = Sigma_rel(x,y)"
-proof (intro iffI)
-  assume "d = Sigma_rel(x,y)"
-  moreover
-  note assms
-  moreover from this
-  obtain e where "M(e)" "is_Sigma(M,x,y,e)"
-    using is_Sigma_witness[of _ y] by blast
-  ultimately
-  show "is_Sigma(M, x, y, d)"
-    using is_Sigma_uniqueness[of x y] is_Sigma_witness
-      theI[OF ex1I[of "\<lambda>d. M(d) \<and> is_Sigma(M,x,y,d)"], OF _ is_Sigma_uniqueness[of x y], of e]
-    unfolding Sigma_rel_def
-    by auto
-next
-  assume "is_Sigma(M, x, y, d)"
-  with assms
-  show "d = Sigma_rel(x,y)"
-    using is_Sigma_uniqueness[of _ y] unfolding Sigma_rel_def
-    by (blast del:the_equality intro:the_equality[symmetric])
+    "is_Sigfun(M,x,B,Sd) \<longleftrightarrow> Sd = Sigfun(x,B)"
+proof -
+  have "\<Union>{z . y \<in> B(x), z = {\<langle>x, y\<rangle>}} = (\<Union>y\<in>B(x). {\<langle>x, y\<rangle>})" by auto
+  then
+  show ?thesis
+    using assms transM[OF _ \<open>M(B(x))\<close>] Replace_abs
+    unfolding is_Sigfun_def Sigfun_def by auto
 qed
 
+lemma Sigfun_closed:
+  assumes
+    "strong_replacement(M, \<lambda>y z. y \<in> B(x) \<and> z = {\<langle>x, y\<rangle>})" 
+    "M(x)" "M(B(x))"
+  shows
+    "M(Sigfun(x,B))"
+  using assms transM[OF _ \<open>M(B(x))\<close>] RepFun_closed2
+  unfolding Sigfun_def by simp
 
-lemma def_Sigma_rel: \<comment> \<open>FAKE!\<close>
-  assumes "M(A)" "(\<And>i. M(i) \<Longrightarrow> M(B(i)))"
-  shows "Sigma_rel(A,B) = Sigma(A,B)"
-  using assms Sigma_rel_iff Sigma_rel_closed
-  unfolding is_Sigma_def by simp
+end (* M_trivial *)
 
-end (* M_basic *)
+definition
+  is_Sigma :: "[i\<Rightarrow>o,i,i\<Rightarrow>i,i]\<Rightarrow>o"  where
+  "is_Sigma(M,A,B,S) \<equiv> \<exists>RSf[M]. 
+      is_Replace(M,A,\<lambda>x z. z=Sigfun(x,B),RSf) \<and> big_union(M,RSf,S)"
 
-(******************  end Discipline  **********************)
+locale M_Pi = M_basic +
+  fixes A B 
+  assumes     
+    Pi_assumptions: 
+    "\<forall>x\<in>A. strong_replacement(M, \<lambda>y z. y \<in> B(x) \<and> z = {\<langle>x, y\<rangle>})" 
+    "strong_replacement(M,\<lambda>x z. z=Sigfun(x,B))" 
+    "M(A)" 
+    "\<forall>x\<in>A. M(B(x))"
+begin
+
+lemma Sigma_abs:
+  assumes
+    "M(S)"
+  shows
+    "is_Sigma(M,A,B,S) \<longleftrightarrow> S = Sigma(A,B)"
+proof -
+  have "\<Union>{z . x \<in> A, z = Sigfun(x, B)} = (\<Union>x\<in>A. Sigfun(x, B))"
+    by auto
+  with assms
+  show ?thesis
+    using Replace_abs[of A _ "\<lambda>x z. z=Sigfun(x,B)"]
+      Sigfun_closed Sigma_Sigfun[of A B] transM[of _ A]
+      Pi_assumptions
+    unfolding is_Sigma_def by simp
+qed
+
+lemma Sigma_closed: "M(Sigma(A,B))"
+proof -
+  have "(\<Union>x\<in>A. Sigfun(x, B)) = \<Union>{z . x \<in> A, z = Sigfun(x, B)}"
+    by auto
+  then
+  show ?thesis
+    using Sigma_Sigfun[of A B] transM[of _ A] 
+      Sigfun_closed Pi_assumptions
+    by simp
+qed
+
+end (* M_Pi *)
 
 (**********************************************************)
 subsection\<open>Discipline for \<^term>\<open>Pi\<close>\<close>
 
 definition (* completely relational *)
-  is_Pi   :: "[i\<Rightarrow>o,i,i\<Rightarrow>i,i]\<Rightarrow>o"  where
-  "is_Pi(M,A,B,I) \<equiv> \<exists>F[M]. is_Sigma(M,A,B,F) \<and>
-       is_Collect(M,F,PiP_rel(M,A),I)"
+  is_Pi :: "[i\<Rightarrow>o,i,i\<Rightarrow>i,i]\<Rightarrow>o"  where
+  "is_Pi(M,A,B,I) \<equiv> \<exists>S[M]. \<exists>PS[M]. is_Sigma(M,A,B,S) \<and> 
+       is_Pow(M,S,PS) \<and>
+       is_Collect(M,PS,PiP_rel(M,A),I)"
 
-context M_basic
+context M_Pi
 begin
 
 lemma is_Pi_uniqueness:
   assumes
-    "M(r)" "(\<And>i. M(i) \<Longrightarrow> M(B(i)))" "M(d)" "M(d')"
-    "is_Pi(M,r,B,d)" "is_Pi(M,r,B,d')"
+    "M(d)" "M(d')"
+    "is_Pi(M,A,B,d)" "is_Pi(M,A,B,d')"
   shows
     "d=d'"
-  using assms Sigma_rel_iff extensionality_trans
+  using assms Sigma_abs extensionality_trans Pi_assumptions
+    Pow_rel_iff
   unfolding is_Pi_def by simp
 
-lemma is_Pi_witness: "M(r) \<Longrightarrow> (\<And>i. M(i) \<Longrightarrow> M(B(i))) \<Longrightarrow> \<exists>d[M]. is_Pi(M,r,B,d)"
-  unfolding is_Pi_def PiP_rel_def
+lemma is_Pi_witness: "\<exists>d[M]. is_Pi(M,A,B,d)"
+  using Pi_assumptions Pow_rel_iff Pow_rel_closed
+    Sigma_closed Sigma_abs
+  unfolding is_Pi_def PiP_rel_def apply simp
   sorry \<comment> \<open>We have to do this by hand, assuming axiom instance for \<^term>\<open>M\<close>\<close>
 
-definition
+end (* M_Pi *)
+
+definition (in M_basic)
   Pi_rel :: "i \<Rightarrow> (i\<Rightarrow>i) \<Rightarrow> i"  where
   "Pi_rel(A,B) \<equiv> THE d. M(d) \<and> is_Pi(M,A,B,d)"
 
-lemma Pi_rel_closed: "M(x) \<Longrightarrow> (\<And>i. M(i) \<Longrightarrow> M(y(i))) \<Longrightarrow> M(Pi_rel(x,y))"
+context M_Pi
+begin
+  \<comment> \<open>From this point on, the higher order variable \<^term>\<open>y\<close> must be
+explicitly instantiated, and proof methods are slower\<close>
+lemma Pi_rel_closed: "M(Pi_rel(A,B))"
   unfolding Pi_rel_def
-  using theI[OF ex1I[of "\<lambda>d. M(d) \<and> is_Pi(M,x,y,d)"], OF _ is_Pi_uniqueness[of x y]]
-    is_Pi_witness[of _ y] by fastforce
+  using theI[OF ex1I[of "\<lambda>d. M(d) \<and> is_Pi(M,A,B,d)"], OF _ is_Pi_uniqueness]
+    is_Pi_witness by auto
 
 lemma Pi_rel_iff:
-  assumes "M(x)" "(\<And>i. M(i) \<Longrightarrow> M(y(i)))" "M(d)"
-  shows "is_Pi(M,x,y,d) \<longleftrightarrow> d = Pi_rel(x,y)"
+  assumes "M(d)"
+  shows "is_Pi(M,A,B,d) \<longleftrightarrow> d = Pi_rel(A,B)"
 proof (intro iffI)
-  assume "d = Pi_rel(x,y)"
+  assume "d = Pi_rel(A,B)"
   moreover
   note assms
   moreover from this
-  obtain e where "M(e)" "is_Pi(M,x,y,e)"
-    using is_Pi_witness[of _ y] by blast
+  obtain e where "M(e)" "is_Pi(M,A,B,e)"
+    using is_Pi_witness by blast
   ultimately
-  show "is_Pi(M, x, y, d)"
-    using is_Pi_uniqueness[of x y] is_Pi_witness
-      theI[OF ex1I[of "\<lambda>d. M(d) \<and> is_Pi(M,x,y,d)"], OF _ is_Pi_uniqueness[of x y], of e]
+  show "is_Pi(M, A, B, d)"
+    using is_Pi_uniqueness is_Pi_witness
+      theI[OF ex1I[of "\<lambda>d. M(d) \<and> is_Pi(M,A,B,d)"], OF _ is_Pi_uniqueness, of e]
     unfolding Pi_rel_def
     by auto
 next
-  assume "is_Pi(M, x, y, d)"
+  assume "is_Pi(M, A, B, d)"
   with assms
-  show "d = Pi_rel(x,y)"
-    using is_Pi_uniqueness[of _ y] unfolding Pi_rel_def
+  show "d = Pi_rel(A,B)"
+    using is_Pi_uniqueness unfolding Pi_rel_def
     by (blast del:the_equality intro:the_equality[symmetric])
 qed
 
 lemma def_Pi_rel:
-  assumes "M(A)" "(\<And>i. M(i) \<Longrightarrow> M(B(i)))"
-  shows "Pi_rel(A,B) =
-         {f \<in> Sigma_rel(A,B).  A \<subseteq> domain(f) \<and> function(f)}"
-         (is "_ = Collect(_,?P)")
+  "Pi_rel(A,B) = {f\<in>Pow_rel(Sigma(A,B)). A\<subseteq>domain(f) \<and> function(f)}"
 proof -
-  from assms
-  have "Pi_rel(A, B) \<subseteq> Sigma_rel(A,B)"
-    using Pi_rel_iff[of A B "Pi_rel(A,B)"] Pi_rel_closed Sigma_rel_iff
+  have "Pi_rel(A, B) \<subseteq> Pow_rel(Sigma(A,B))"
+    using Pi_rel_iff[of "Pi_rel(A,B)"] Pi_rel_closed Sigma_abs
+      Pi_assumptions Pow_rel_iff
     unfolding is_Pi_def by auto
-  moreover from assms
-  have "f \<in> Pi_rel(A, B) \<Longrightarrow> ?P(f)" for f
-    using Pi_rel_iff[of A B "Pi_rel(A,B)"] Pi_rel_closed Sigma_rel_iff
-      def_PiP_rel transM[OF _ Sigma_rel_closed, OF _ \<open>M(A)\<close>]
-    unfolding is_Pi_def by auto
-  moreover from assms
-  have "f \<in> Sigma_rel(A,B) \<Longrightarrow> ?P(f) \<Longrightarrow> f \<in> Pi_rel(A, B)" for f
-    using Pi_rel_iff[of A B "Pi_rel(A,B)"] Pi_rel_closed Sigma_rel_iff
-      def_PiP_rel transM[OF _ Sigma_rel_closed, OF _ \<open>M(A)\<close>]
-    unfolding is_Pi_def by auto
+  moreover \<comment> \<open>Note the use of transitivity here:\<close>
+  have "f \<in> Pi_rel(A, B) \<Longrightarrow> A\<subseteq>domain(f) \<and> function(f)" for f
+    using Pi_rel_iff[of "Pi_rel(A,B)"] Pi_rel_closed Sigma_abs
+      Pi_assumptions def_PiP_rel[of A f] transM[OF _ Sigma_closed] 
+      transM[OF _ Pi_rel_closed] Pow_rel_iff
+      \<comment> \<open>Closure and absoluteness should be in the simpset in this
+      context\<close>
+    unfolding is_Pi_def by simp
+  moreover
+  have "f \<in> Pow_rel(Sigma(A,B)) \<Longrightarrow> A\<subseteq>domain(f) \<and> function(f) \<Longrightarrow> f \<in> Pi_rel(A, B)" for f
+    using Pi_rel_iff[of "Pi_rel(A,B)"] Pi_rel_closed Sigma_abs 
+      transM[OF _ Sigma_closed]  Pi_assumptions
+      def_PiP_rel[of A f, symmetric] transM[OF _ Sigma_closed] 
+      transM[OF _ Pi_rel_closed] Sigma_closed[THEN Pow_rel_closed, THEN [2] transM, of f]
+      Pow_rel_iff
+    unfolding is_Pi_def by simp
   ultimately
   show ?thesis by force
 qed
-end (* M_basic *)
+end (* M_Pi *)
 
 (******************  end Discipline  **********************)
-
 
 end
