75edee58c6c5032754f09c7f350e5623ac63187f
sterraf <sterraf@users.noreply.github.com>
2020-12-22 15:48:11 -0300

    Updating ported results: applying automatic tools up to ordertype

--- src/New_Discipline_Draft.thy
+++ src/New_Discipline_Draft.thy
@@ -179,16 +179,35 @@ end
 
 text\<open>Esa es toda la disciplina.\<close>
 
+reldb_add relational "Ord" "ordinal"
+reldb_add functional "lt" "lt"
+reldb_add relational "lt" "lt_rel"
+synthesize "lt_rel" from_definition
+arity_theorem intermediate for "lt_rel_fm"
+
+lemma arity_lt_rel_fm[arity]: "a \<in> nat \<Longrightarrow> b \<in> nat \<Longrightarrow> arity(lt_rel_fm(a, b)) = succ(a) \<union> succ(b)"
+  using arity_lt_rel_fm'
+  by auto
+
 relativize functional "Card" "Card_rel" external
+relationalize "Card_rel" "is_Card"
+synthesize "is_Card" from_definition assuming "nonempty"
+arity_theorem for "is_Card_fm"
 
 notation Card_rel (\<open>Card\<^bsup>_\<^esup>'(_')\<close>)
+
+lemma (in M_Perm) is_Card_iff: "M(A) \<Longrightarrow> is_Card(M, A) \<longleftrightarrow> Card\<^bsup>M\<^esup>(A)"
+  using is_cardinal_iff
+  unfolding is_Card_def Card_rel_def by simp
 
 abbreviation
   Card_r_set  :: "[i,i]\<Rightarrow>o"  (\<open>Card\<^bsup>_\<^esup>'(_')\<close>) where
   "Card\<^bsup>M\<^esup>(i) \<equiv> Card_rel(##M,i)"
 
-reldb_add functional "lt" "lt"
 relativize functional "InfCard" "InfCard_rel" external
+relationalize "InfCard_rel" "is_InfCard"
+synthesize "is_InfCard" from_definition assuming "nonempty"
+arity_theorem for "is_InfCard_fm"
 
 notation InfCard_rel (\<open>InfCard\<^bsup>_\<^esup>'(_')\<close>)
 
@@ -258,6 +277,7 @@ is_iff_rel for "cmult"
 
 end
 
+
 definition
   Powapply :: "[i,i] \<Rightarrow> i"  where
   "Powapply(f,y) \<equiv> Pow(f`y)"
@@ -265,10 +285,12 @@ definition
 reldb_add functional "Pow" "Pow_rel"
 reldb_add relational "Pow" "is_Pow"
 
-lemma subset_fm_iff_sats[iff_sats]:
+lemma subset_iff_sats[iff_sats]:
   "nth(i, env) = x \<Longrightarrow> nth(j, env) = y \<Longrightarrow> i\<in>nat \<Longrightarrow> j\<in>nat \<Longrightarrow>
    env \<in> list(A) \<Longrightarrow> subset(##A, x, y) \<longleftrightarrow> A, env \<Turnstile> subset_fm(i, j)"
-  using sats_subset_fm'  by simp
+  using sats_subset_fm' by simp
+
+declare Replace_iff_sats[iff_sats]
 
 synthesize "is_Pow" from_definition assuming "nonempty"
 arity_theorem for "is_Pow_fm"
@@ -299,30 +321,34 @@ univalent for "Powapply"
 
 end
 
-definition
+(* definition
   HVfrom_rel :: "[i\<Rightarrow>o,i,i,i] \<Rightarrow> i" (\<open>HVfrom\<^bsup>_\<^esup>'(_,_,_')\<close>) where
-  "HVfrom\<^bsup>M\<^esup>(A,x,f) \<equiv> A \<union> (\<Union>y\<in>x. Powapply\<^bsup>M\<^esup>(f,y))"
+  "HVfrom\<^bsup>M\<^esup>(A,x,f) \<equiv> A \<union> (\<Union>y\<in>x. Powapply\<^bsup>M\<^esup>(f,y))" *)
 
-(* relativization *)
 definition
-  is_HVfrom :: "[i\<Rightarrow>o,i,i,i,i] \<Rightarrow> o" where
-  "is_HVfrom(M,A,x,f,z) \<equiv> (\<exists>u[M]. \<exists>hr[M]. is_Replace(M,x,is_Powapply(M,f),hr) \<and> 
-                          big_union(M,hr,u) \<and> union(M,A,u,z))"
+  HVfrom :: "[i,i,i] \<Rightarrow> i" where
+  "HVfrom(A,x,f) \<equiv> A \<union> (\<Union>y\<in>x. Powapply(f,y))"
 
-locale M_HVfrom = M_eclose + 
-  assumes 
-    Powapply_replacement : 
+relativize functional "HVfrom" "HVfrom_rel"
+relationalize "HVfrom_rel" "is_HVfrom"
+synthesize "is_HVfrom" from_definition assuming "nonempty"
+arity_theorem for "is_HVfrom_fm"
+
+notation HVfrom_rel (\<open>HVfrom\<^bsup>_\<^esup>'(_,_,_')\<close>)
+
+locale M_HVfrom = M_eclose +
+  assumes
+    Powapply_replacement:
       "M(K) \<Longrightarrow> strong_replacement(M,\<lambda>y z. z = Powapply\<^bsup>M\<^esup>(f,y))"
-
 begin
 
-lemma is_HVfrom_iff :
-  assumes "M(A)" "M(x)" "M(f)" "M(z)"
-  shows "is_HVfrom(M,A,x,f,z) \<longleftrightarrow> z = HVfrom\<^bsup>M\<^esup>(A,x,f)"
+is_iff_rel for "HVfrom"
 proof -
+  assume assms:"M(A)" "M(x)" "M(f)" "M(res)"
+  then
   have "is_Replace(M,x,\<lambda>y z. z = Powapply\<^bsup>M\<^esup>(f,y),r) \<longleftrightarrow> r = {z . y\<in>x, z = Powapply\<^bsup>M\<^esup>(f,y)}"
     if "M(r)" for r
-    using assms that Powapply_rel_closed 
+    using that Powapply_rel_closed
           Replace_abs[of x r "\<lambda>y z. z = Powapply\<^bsup>M\<^esup>(f,y)"] transM[of _ x]
     by simp
   moreover
@@ -340,18 +366,25 @@ proof -
     using assms strong_replacement_closed[OF Powapply_replacement] 
           Powapply_rel_closed transM[of _ x]
     by simp
+  moreover from assms
+  \<comment> \<open>intermediate step for body of Replace\<close>
+  have "{z . y \<in> x, z = Powapply\<^bsup>M\<^esup>(f,y)} = {y . w \<in> x, M(y) \<and> M(w) \<and> y = Powapply\<^bsup>M\<^esup>(f,w)}" 
+    by (auto dest:transM)
   ultimately
   show ?thesis
-  using assms 
+    using assms
   unfolding is_HVfrom_def HVfrom_rel_def
-  by auto
+    by (auto dest:transM)
 qed
 
+univalent for "HVfrom"
+  using is_HVfrom_iff
+  unfolding univalent_def
+  by simp
 
-lemma HVfrom_rel_closed: 
-  assumes "M(A)" "M(x)" "M(f)"
-  shows "M(HVfrom\<^bsup>M\<^esup>(A,x,f))"
+rel_closed for "HVfrom"
 proof -
+  assume assms:"M(A)" "M(x)" "M(f)"
   have "M({z . y \<in> x, z = Powapply\<^bsup>M\<^esup>(f,y)})" 
     using assms strong_replacement_closed[OF Powapply_replacement] 
           Powapply_rel_closed transM[of _ x]
@@ -360,11 +393,10 @@ proof -
   have "M(A \<union> \<Union>({z . y\<in>x, z = Powapply\<^bsup>M\<^esup>(f,y)}))"
     using assms
     by simp
-  moreover
-  have "A \<union> \<Union>({z . y\<in>x, z = Powapply\<^bsup>M\<^esup>(f,y)}) =
-        HVfrom\<^bsup>M\<^esup>(A,x,f)" 
-    unfolding HVfrom_rel_def
-    by auto
+  moreover from assms
+  \<comment> \<open>intermediate step for body of Replace\<close>
+  have "{z . y \<in> x, z = Powapply\<^bsup>M\<^esup>(f,y)} = {y . w \<in> x, M(y) \<and> M(w) \<and> y = Powapply\<^bsup>M\<^esup>(f,w)}" 
+    by (auto dest:transM)
   ultimately
   show ?thesis
     using assms
@@ -372,7 +404,34 @@ proof -
     by simp    
 qed
 
-end (* context M_HVfrom *)
+end (*  M_HVfrom *)
+
+(*
+relativize functional "Vfrom" "Vfrom_rel" external
+relationalize "Vfrom_rel" "is_Vfrom"
+synthesize "is_Vfrom" from_definition assuming "nonempty"
+arity_theorem for "is_Vfrom_fm"
+
+notation Vfrom_rel (\<open>Vfrom\<^bsup>_\<^esup>'(_,_')\<close>)
+
+context M_basic
+begin
+
+is_iff_rel for "Vfrom"
+  using Pow_rel_iff
+  unfolding is_Vfrom_def Vfrom_rel_def
+  by simp
+
+univalent for "Vfrom"
+  using is_Vfrom_iff
+  unfolding univalent_def
+  by simp
+
+rel_closed for "Vfrom"
+  unfolding Vfrom_rel_def
+  by simp
+
+*)
 
 definition
   Vfrom_rel :: "[i\<Rightarrow>o,i,i] \<Rightarrow> i" (\<open>Vfrom\<^bsup>_\<^esup>'(_,_')\<close>) where
