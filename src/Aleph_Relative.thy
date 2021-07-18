01cf5c9977b7ff847e1d601a6a95a3a2bb31942d
sterraf <sterraf@users.noreply.github.com>
2021-07-18 18:53:13 -0300

    sats_is_Aleph_fm_auto using is_transrec_iff_sats

--- src/Aleph_Relative.thy
+++ src/Aleph_Relative.thy
@@ -27,28 +27,21 @@ definition
 relativize functional "Aleph'" "Aleph_rel"
 relationalize "Aleph_rel" "is_Aleph"
 
-lemma is_transrec_iff_sats':
-  assumes MH_iff_sats:
-    "!!a0 a1 a2 a3 a4 a5 a6 a7.
-        [|a0\<in>A; a1\<in>A; a2\<in>A; a3\<in>A; a4\<in>A; a5\<in>A; a6\<in>A; a7\<in>A|]
-        ==> MH(a2, a1, a0) \<longleftrightarrow>
-            sats(A, p, Cons(a0,Cons(a1,Cons(a2,Cons(a3,
-                          Cons(a4,Cons(a5,Cons(a6,Cons(a7,env)))))))))" "0\<in>A"
-  shows
-    "[|nth(i,env) = x; nth(k,env) = z; env \<in> list(A)|]
-   ==> is_transrec(##A, MH, x, z) \<longleftrightarrow> sats(A, is_transrec_fm(p,i,k), env)"
-  using is_transrec_iff_sats
-    \<comment> \<open>cases on whether the assumptions of original theorem are satisfied\<close>
-  sorry
-
-manual_schematic for "is_Aleph" assuming "nonempty"
+txt\<open>The extra assumptions \<^term>\<open>a < length(env)\<close> and \<^term>\<open>a < length(env)\<close>
+    in this schematic goal (and the following results on synthesis that
+    depend on it) are imposed by @{thm is_transrec_iff_sats}.\<close>
+schematic_goal sats_is_Aleph_fm_auto:
+  "a \<in> nat \<Longrightarrow> c \<in> nat \<Longrightarrow> env \<in> list(A) \<Longrightarrow> 
+  a < length(env) \<Longrightarrow> c < length(env) \<Longrightarrow> 0 \<in> A \<Longrightarrow> 
+  is_Aleph(##A, nth(a, env), nth(c, env)) \<longleftrightarrow> A, env \<Turnstile> ?fm(a, c)"
   unfolding is_Aleph_def
-  apply (rule is_transrec_iff_sats')
+  apply (rule is_transrec_iff_sats)
 proof (rule is_HAleph_iff_sats)
   fix a0 a1 a2 a3 a4 a5 a6 a7
   show "nth(2, Cons(a0, Cons(a1, Cons(a2, Cons(a3, Cons(a4, Cons(a5, Cons(a6, Cons(a7, env))))))))) = a2"
     "nth(1, Cons(a0, Cons(a1, Cons(a2, Cons(a3, Cons(a4, Cons(a5, Cons(a6, Cons(a7, env))))))))) = a1"
     "nth(0, Cons(a0, Cons(a1, Cons(a2, Cons(a3, Cons(a4, Cons(a5, Cons(a6, Cons(a7, env))))))))) = a0"
+    "nth(c, env) = nth(c, env)"
     by simp_all
 qed simp_all
 
@@ -60,13 +53,13 @@ lemma is_Aleph_fm_type [TC]: "a \<in> nat \<Longrightarrow> c \<in> nat \<Longri
   unfolding is_Aleph_fm_def by simp
 
 lemma sats_is_Aleph_fm:
-  assumes "f\<in>nat" "r\<in>nat" "env \<in> list(A)" "0\<in>A"
+  assumes "f\<in>nat" "r\<in>nat" "env \<in> list(A)" "0\<in>A" "f < length(env)" "r< length(env)"
   shows "is_Aleph(##A, nth(f, env), nth(r, env)) \<longleftrightarrow> A, env \<Turnstile> is_Aleph_fm(f,r)"
   using assms sats_is_Aleph_fm_auto unfolding is_Aleph_def is_Aleph_fm_def by simp
 
 lemma is_Aleph_iff_sats [iff_sats]:
   assumes
-    "nth(f, env) = fa" "nth(r, env) = ra"
+    "nth(f, env) = fa" "nth(r, env) = ra" "f < length(env)" "r< length(env)"
     "f \<in> nat" "r \<in> nat" "env \<in> list(A)" "0\<in>A"
   shows "is_Aleph(##A,fa,ra) \<longleftrightarrow> A, env \<Turnstile> is_Aleph_fm(f,r)"
   using assms sats_is_Aleph_fm[of f r env A] by simp
