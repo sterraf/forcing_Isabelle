99eda1ee0bb41c3871e94ef873a6fa6317baf675
sterraf <sterraf@users.noreply.github.com>
2021-07-25 23:43:04 -0300

    added modified id_separation, new repl instances derived

--- src/Replacement_Derivations.thy
+++ src/Replacement_Derivations.thy
@@ -3,6 +3,7 @@ section\<open>Main definitions of the development\<close>
 theory Replacement_Derivations
   imports
     "ZF-Constructible.Relative"
+    CardinalArith_Relative
     "../Tools/Try0"
 begin
 
@@ -16,6 +17,8 @@ locale M_replacement = M_basic +
     and
     snd_lam_replacement: "lam_replacement(M,snd)"
     and
+    id_separation:"M(A) \<Longrightarrow> separation(M, \<lambda>z. \<exists>x[M]. z = \<langle>x, x\<rangle>)"
+    and
     middle_separation: "separation(M, \<lambda>x. snd(fst(x))=fst(snd(x)))"
     and
     middle_del_replacement: "strong_replacement(M, \<lambda>x y. y=\<langle>fst(fst(x)),snd(snd(x))\<rangle>)"
@@ -27,13 +30,15 @@ locale M_replacement = M_basic +
 begin
 
 lemma lam_replacement_iff_lam_closed:
-  assumes "\<forall>x. M(b(x))"
+  assumes "\<forall>x[M]. M(b(x))"
   shows "lam_replacement(M, b) \<longleftrightarrow>  (\<forall>A[M]. M(\<lambda>x\<in>A. b(x)))"
   using assms lam_closed lam_funtype[of _ b, THEN Pi_memberD]
-  unfolding lam_replacement_def strong_replacement_def by (auto intro:lamI)
+  unfolding lam_replacement_def strong_replacement_def
+  by (auto intro:lamI dest:transM)
+    (rule lam_closed, auto simp add:strong_replacement_def dest:transM)
 
 lemma lam_replacement_imp_strong_replacement:
-  assumes "lam_replacement(M, b)" "\<forall>x. M(b(x))"
+  assumes "lam_replacement(M, b)" "\<forall>x[M]. M(b(x))"
   shows "strong_replacement(M, \<lambda>x y. y = b(x))"
 proof -
   {
@@ -62,7 +67,7 @@ lemma RepFun_middle_del: "{ \<langle>fst(fst(p)),snd(snd(p))\<rangle> . p \<in> 
 
 lemma lam_replacement_hcomp:
   assumes "lam_replacement(M,f)" "lam_replacement(M,g)"
-          "\<forall>x. M(f(x))" "\<forall>x. M(g(x))"
+          "\<forall>x[M]. M(f(x))" "\<forall>x[M]. M(g(x))"
   shows "lam_replacement(M, \<lambda>x. g(f(x)))"
 proof -
   {
@@ -72,7 +77,7 @@ proof -
     assume "M(A)"
     moreover from calculation
     have "M({f(x) . x\<in>A})"
-      using RepFun_closed[of f] lam_replacement_imp_strong_replacement by auto
+      using RepFun_closed[of f A] lam_replacement_imp_strong_replacement by (auto dest:transM)
     moreover from calculation
     have "M((\<lambda>x\<in>A. f(x)) \<times> (\<lambda>x\<in>{f(x) . x\<in>A}. g(x)))"
       using lam_replacement_iff_lam_closed by simp
@@ -107,7 +112,7 @@ lemma RepFun_pullback:
 
 lemma lam_replacement_pullback:
   assumes "lam_replacement(M,f)" "lam_replacement(M,g)"
-          "\<forall>x. M(f(x))" "\<forall>x. M(g(x))"
+          "\<forall>x[M]. M(f(x))" "\<forall>x[M]. M(g(x))"
   shows "lam_replacement(M, \<lambda>x. \<langle>f(x),g(x)\<rangle>)"
 proof -
   {
@@ -117,7 +122,8 @@ proof -
     assume "M(A)"
     moreover from calculation
     have "M({f(x) . x\<in>A})" "M({g(x) . x\<in>A})"
-      using RepFun_closed lam_replacement_imp_strong_replacement by auto
+      using RepFun_closed[of f A] RepFun_closed[of g A]
+        lam_replacement_imp_strong_replacement by (auto dest:transM)
     moreover from calculation
     have "M((\<lambda>x\<in>A. f(x)) \<times> (\<lambda>x\<in>A. g(x)))"
       using lam_replacement_iff_lam_closed by simp
@@ -141,6 +147,43 @@ proof -
   show ?thesis using lam_replacement_iff_lam_closed by simp
 qed
 
+lemma lam_replacement_identity: "lam_replacement(M,\<lambda>x. x)"
+proof -
+  {
+    fix A
+      \<comment> \<open>From here on, modified proof of @{thm M_cardinals.id_closed},
+          the modified locale assumption @{thm id_separation}\<close>
+    assume "M(A)"
+    moreover from this
+    have "id(A) = {z\<in> A\<times>A. \<exists>x[M]. z=<x,x>}"
+      unfolding id_def lam_def by (auto dest:transM)
+    moreover from calculation
+    have "M({z\<in> A\<times>A. \<exists>x[M]. z=<x,x>})"
+      using id_separation by simp
+    ultimately
+    have "M(id(A))" by simp
+  }
+  then
+  show ?thesis using lam_replacement_iff_lam_closed
+    unfolding id_def by simp
+qed
+
+lemma lam_replacement_constant: "M(b) \<Longrightarrow> lam_replacement(M,\<lambda>_. b)"
+  unfolding lam_replacement_def strong_replacement_def
+  by safe (rule_tac x="_\<times>{b}" in rexI; blast)
+
+lemma id_replacement: "strong_replacement(M, \<lambda>x y. y = \<langle>x, x, x\<rangle>)"
+  using lam_replacement_identity lam_replacement_pullback[of "\<lambda>x. x" "\<lambda>x. x"]
+  unfolding lam_replacement_def by simp
+
+lemma pospend_replacement: "M(b) \<Longrightarrow> strong_replacement(M, \<lambda>x y. y = \<langle>x, x, b\<rangle>)"
+  using lam_replacement_identity lam_replacement_constant
+    lam_replacement_pullback[of "\<lambda>x. x" "\<lambda>x. b"]
+  unfolding lam_replacement_def by simp
+
 end (* M_replacement *)
+
+find_theorems "strong_replacement(_,\<lambda>x y. y = <x,_>)" -name: Derivations
+-"strong_replacement(_,\<lambda>x y. y = <x,_>) \<Longrightarrow> _" -name:"_def" -name:intro
 
 end
