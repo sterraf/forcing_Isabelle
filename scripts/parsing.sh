c0dac21f289950f347d5f9901cf9ba5b64dd0d9a
sterraf <sterraf@users.noreply.github.com>
2021-08-25 22:31:55 -0300

    slightly better parsing, TOC TOC

--- scripts/parsing.sh
+++ scripts/parsing.sh
@@ -2,35 +2,40 @@
 
 session=$1
 
+function parse_basic {
+    #
+    # Parse item
+    #
+    echo -n Parsing \"$1\" items...
+    for x in *.html; do y=`basename -s".html" $x`; sed -i -e "h;s|.*$1</span></span>[ ]\+\([^<>/ ]\+\)[ ]*<span.*|$y.GBL.\1|w lemas__$y.txt" -e "g;s|<span \(class=\"command\">$1</span></span> \)\([^<>/ ]\+\)[ ]*<span|<span id=\"$y.GBL.\2\" \1\2 <span|" $x; done; cat lemas__*txt >> lemas_$session.txt; rm -f lemas__*txt
+    echo  Done
+}
+
+function parse_in_locale {
+    echo -n Parsing \"$1 "(in locale)"\" items...
+    for x in *.html; do y=`basename -s".html" $x`; sed -i -e "h;s|.*$1</span></span>[ ]\+\([^)]\+([^)]\+>in<[^)]\+\)\(<span class=\"main\">)</span>[ ]*\)\([^<>/ ]\+\)[ ]*<span.*|$y.GBL.\3|w lemas__$y.txt" -e "g;s|<span \(class=\"command\">$1</span></span>[ ]*[^)]*([^)]\+>in<[^)]\+<span class=\"main\">)</span>[ ]*\)\([^<>/ ]\+\)[ ]*<span|<span id=\"$y.GBL.\2\" \1\2 <span|" $x; done; cat lemas__*txt >> lemas_$session.txt; rm -f lemas__*txt
+    echo  Done
+}
+
 ## Script de indizado y linkeo de definiciones completo
 echo -n Parsing "definition" items...
 for x in *.html; do y=`basename -s".html" $x`; sed -i -e "h;s|.*<span class=\"entity\">\([^<>/ ]*\)</span>[ ]*<span class=\"main\">::.*|$y.GBL.\1|w definiciones__$y.txt" -e "g;s|<span\( class=\"entity\">\)\([^<>/ ]*\)\(</span>[ ]*<span class=\"main\">::\)|<span id=\"$y.GBL.\2\"\1\2\3|" $x ; done; cat definiciones__*txt > definiciones_$session.txt ; rm -f definiciones__*txt
 echo  Done
 
 ## Script de indizado y linkeo de lemas
-echo -n Parsing "lemma" items...
-for x in *.html; do y=`basename -s".html" $x`; sed -i -e "h;s|.*lemma</span></span>[ ]\+\([^<>/ ]\+\)[ ]*<span.*|$y.GBL.\1|w lemas__$y.txt" -e "g;s|<span \(class=\"command\">lemma</span></span> \)\([^<>/ ]\+\)[ ]*<span|<span id=\"$y.GBL.\2\" \1\2 <span|" $x; done; cat lemas__*txt > lemas_$session.txt; rm -f lemas__*txt
-echo  Done
+parse_basic "lemma"
 
 ## Script de indizado y linkeo de lemas en locales
-echo -n Parsing "lemma (in locale)" items...
-for x in *.html; do y=`basename -s".html" $x`; sed -i -e "h;s|.*lemma</span></span>[ ]\+\([^)]\+([^)]\+>in<[^)]\+\)\(<span class=\"main\">)</span>[ ]*\)\([^<>/ ]\+\)[ ]*<span.*|$y.GBL.\3|w lemas__$y.txt" -e "g;s|<span \(class=\"command\">lemma</span></span>[ ]*[^)]*([^)]\+>in<[^)]\+<span class=\"main\">)</span>[ ]*\)\([^<>/ ]\+\)[ ]*<span|<span id=\"$y.GBL.\2\" \1\2 <span|" $x; done; cat lemas__*txt >> lemas_$session.txt; rm -f lemas__*txt
-echo  Done
+parse_in_locale "lemma"
 
 ## Script de indizado y linkeo de teoremas
-echo -n Parsing "theorem" items...
-for x in *.html; do y=`basename -s".html" $x`; sed -i -e "h;s|.*theorem</span></span>[ ]\+\([^<>/ ]\+\)[ ]*<span.*|$y.GBL.\1|w lemas__$y.txt" -e "g;s|<span \(class=\"command\">theorem</span></span> \)\([^<>/ ]\+\)[ ]*<span|<span id=\"$y.GBL.\2\" \1\2 <span|" $x; done; cat lemas__*txt >> lemas_$session.txt; rm -f lemas__*txt
-echo  Done
+parse_basic "theorem"
 
 ## Script de indizado y linkeo de teoremas en locales
-echo -n Parsing "theorem (in locale)" items...
-for x in *.html; do y=`basename -s".html" $x`; sed -i -e "h;s|.*theorem</span></span>[ ]\+\([^)]\+([^)]\+>in<[^)]\+\)\(<span class=\"main\">)</span>[ ]*\)\([^<>/ ]\+\)[ ]*<span.*|$y.GBL.\3|w lemas__$y.txt" -e "g;s|<span \(class=\"command\">theorem</span></span>[ ]*[^)]*([^)]\+>in<[^)]\+<span class=\"main\">)</span>[ ]*\)\([^<>/ ]\+\)[ ]*<span|<span id=\"$y.GBL.\2\" \1\2 <span|" $x; done; cat lemas__*txt >> lemas_$session.txt; rm -f lemas__*txt
-echo  Done
+parse_in_locale "theorem"
 
 ## Script de indizado y linkeo de goles esquematicos
-echo -n Parsing "schematic_goal" items...
-for x in *.html; do y=`basename -s".html" $x`; sed -i -e "h;s|.*schematic_goal</span></span>[ ]\+\([^<>/ ]\+\)[ ]*<span.*|$y.GBL.\1|w lemas__$y.txt" -e "g;s|<span \(class=\"command\">schematic_goal</span></span> \)\([^<>/ ]\+\)[ ]*<span|<span id=\"$y.GBL.\2\" \1\2 <span|" $x; done; cat lemas__*txt >> lemas_$session.txt; rm -f lemas__*txt
-echo  Done
+parse_basic "schematic_goal"
 
 ## Script de indizado y linkeo de "lemmas" (sic)
 echo -n Parsing "lemmas" items...
