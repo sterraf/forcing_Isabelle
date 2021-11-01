4413503210c9e40bbed742effd57c08552ec78ba
Pedro Sánchez Terraf <pedrost@gmail.com>
2021-11-01 19:17:03 -0300

    advances in porting Pow_rel_eqpoll_rel_function_space_rel

--- src/ZF_Library_Relative2.thy
+++ src/ZF_Library_Relative2.thy
@@ -79,33 +79,6 @@ lemma function_space_rel_eqpoll_rel_cong:
 qed
 *)
 
-lemma curry_eqpoll_rel:
-  fixes \<nu>1 \<nu>2  \<kappa>
-  assumes  "M(\<nu>1)" "M(\<nu>2)" "M(\<kappa>)"
-  shows "\<nu>1 \<rightarrow>\<^bsup>M\<^esup> (\<nu>2 \<rightarrow>\<^bsup>M\<^esup> \<kappa>) \<approx>\<^bsup>M\<^esup> \<nu>1 \<times> \<nu>2 \<rightarrow>\<^bsup>M\<^esup> \<kappa>"
-  unfolding eqpoll_def
-  sorry
-(* proof (intro exI, rule lam_bijective,
-    rule_tac [1-2] lam_type, rule_tac [2] lam_type)
-  fix f z
-  assume "f : \<nu>1 \<rightarrow> \<nu>2 \<rightarrow> \<kappa>" "z \<in> \<nu>1 \<times> \<nu>2"
-  then
-  show "f`fst(z)`snd(z) \<in> \<kappa>"
-    by simp
-next
-  fix f x y
-  assume "f : \<nu>1 \<times> \<nu>2 \<rightarrow> \<kappa>" "x\<in>\<nu>1" "y\<in>\<nu>2"
-  then
-  show "f`\<langle>x,y\<rangle> \<in> \<kappa>" by simp
-next \<comment> \<open>one composition is the identity:\<close>
-  fix f
-  assume "f : \<nu>1 \<times> \<nu>2 \<rightarrow> \<kappa>"
-  then
-  show "(\<lambda>x\<in>\<nu>1 \<times> \<nu>2. (\<lambda>x\<in>\<nu>1. \<lambda>xa\<in>\<nu>2. f ` \<langle>x, xa\<rangle>) ` fst(x) ` snd(x)) = f"
-    by (auto intro:fun_extension)
-qed simp \<comment> \<open>the other composition follows automatically\<close>
- *)
-
 lemma f_imp_injective_rel:
   assumes "f \<in> A \<rightarrow>\<^bsup>M\<^esup> B" "\<forall>x\<in>A. d(f ` x) = x" "M(A)" "M(B)"
   shows "f \<in> inj\<^bsup>M\<^esup>(A, B)"
@@ -154,6 +127,32 @@ lemma lam_bijective_rel:
   apply (blast intro!: lam_injective_rel lam_surjective_rel)
   done
 
+lemma curry_eqpoll_rel:
+  fixes \<nu>1 \<nu>2  \<kappa>
+  assumes  "M(\<nu>1)" "M(\<nu>2)" "M(\<kappa>)"
+  shows "\<nu>1 \<rightarrow>\<^bsup>M\<^esup> (\<nu>2 \<rightarrow>\<^bsup>M\<^esup> \<kappa>) \<approx>\<^bsup>M\<^esup> \<nu>1 \<times> \<nu>2 \<rightarrow>\<^bsup>M\<^esup> \<kappa>"
+  unfolding eqpoll_rel_def
+  sorry
+(* proof (intro rexI, rule lam_bijective_rel
+    rule_tac [1-2] lam_type, rule_tac [2] lam_type)
+  fix f z
+  assume "f : \<nu>1 \<rightarrow> \<nu>2 \<rightarrow> \<kappa>" "z \<in> \<nu>1 \<times> \<nu>2"
+  then
+  show "f`fst(z)`snd(z) \<in> \<kappa>"
+    by simp
+next
+  fix f x y
+  assume "f : \<nu>1 \<times> \<nu>2 \<rightarrow> \<kappa>" "x\<in>\<nu>1" "y\<in>\<nu>2"
+  then
+  show "f`\<langle>x,y\<rangle> \<in> \<kappa>" by simp
+next \<comment> \<open>one composition is the identity:\<close>
+  fix f
+  assume "f : \<nu>1 \<times> \<nu>2 \<rightarrow> \<kappa>"
+  then
+  show "(\<lambda>x\<in>\<nu>1 \<times> \<nu>2. (\<lambda>x\<in>\<nu>1. \<lambda>xa\<in>\<nu>2. f ` \<langle>x, xa\<rangle>) ` fst(x) ` snd(x)) = f"
+    by (auto intro:fun_extension)
+qed simp \<comment> \<open>the other composition follows automatically\<close> *)
+
 lemma Pow_rel_eqpoll_rel_function_space_rel:
   fixes d X
   notes bool_of_o_def [simp]
@@ -161,43 +160,60 @@ lemma Pow_rel_eqpoll_rel_function_space_rel:
     \<comment> \<open>the witnessing map for the thesis:\<close>
   assumes "M(X)"
   shows "Pow\<^bsup>M\<^esup>(X) \<approx>\<^bsup>M\<^esup> X \<rightarrow>\<^bsup>M\<^esup> 2"
-  unfolding eqpoll_rel_def
-proof (intro rexI, rule lam_bijective_rel)
+proof -
   from assms
   interpret M_Pi_assumptions M X "\<lambda>_. 2"
     using Pi_replacement Pi_separation lam_replacement_identity
       lam_replacement_Sigfun[THEN lam_replacement_imp_strong_replacement]
-    apply unfold_locales apply auto
-    sorry
-  \<comment> \<open>We give explicit mutual inverses\<close>
-  fix A
-  assume "A\<in>Pow\<^bsup>M\<^esup>(X)"
-  moreover
-  note \<open>M(X)\<close>
-  moreover from calculation
-  have "M(A)" by (auto dest:transM)
-  ultimately
-  show "d(A) : X \<rightarrow>\<^bsup>M\<^esup> 2"
-    using lam_type_M[of "\<lambda>x. bool_of_o(x\<in>A)"] function_space_rel_char
-      lam_type[of _ "\<lambda>x. bool_of_o(x\<in>A)" "\<lambda>_. 2"]
-    apply (auto)
-    sorry
-  from \<open>A\<in>Pow\<^bsup>M\<^esup>(X)\<close> \<open>M(X)\<close>
-  show "{y\<in>X. d(A)`y = 1} = A"
-    using Pow_rel_char by auto
-next
-  fix f
-  assume "f: X\<rightarrow>\<^bsup>M\<^esup> 2"
+      Pi_replacement1[of _ 2] transM[of _ X] lam_replacement_constant
+    by unfold_locales auto
+  have "lam_replacement(M, \<lambda>x. bool_of_o(x\<in>A))" if "M(A)" for A
+    using that lam_replacement_if lam_replacement_constant
+      separation_in by simp
   with assms
-  have "f: X\<rightarrow> 2" using function_space_rel_char by simp
-  then
-  show "d({y \<in> X . f ` y = 1}) = f"
-    using apply_type[OF \<open>f: X\<rightarrow>2\<close>] by (force intro:fun_extension)
-  show "{ya \<in> X . f ` ya = 1} \<in> Pow\<^bsup>M\<^esup>(X)"
+  have "lam_replacement(M, \<lambda>x. d(x))"
+    using separation_in[THEN [3] lam_replacement_if, of "\<lambda>_.1" "\<lambda>_.0"]
+        lam_replacement_identity lam_replacement_constant
+    apply auto
     sorry
-next
-  show "M(\<lambda>x\<in>Pow\<^bsup>M\<^esup>(X). d(x))" sorry
-qed (auto simp:\<open>M(X)\<close>)
+  show ?thesis
+    unfolding eqpoll_rel_def
+  proof (intro rexI, rule lam_bijective_rel)
+    \<comment> \<open>We give explicit mutual inverses\<close>
+    fix A
+    assume "A\<in>Pow\<^bsup>M\<^esup>(X)"
+    moreover
+    note \<open>M(X)\<close>
+    moreover from calculation
+    have "M(A)" by (auto dest:transM)
+    moreover
+    note \<open>_ \<Longrightarrow> lam_replacement(M, \<lambda>x. bool_of_o(x\<in>A))\<close>
+    ultimately
+    show "d(A) : X \<rightarrow>\<^bsup>M\<^esup> 2"
+      using function_space_rel_char lam_replacement_iff_lam_closed[THEN iffD1]
+      by (simp, rule_tac lam_type[of X "\<lambda>x. bool_of_o(x\<in>A)" "\<lambda>_. 2", simplified])
+        auto
+    from \<open>A\<in>Pow\<^bsup>M\<^esup>(X)\<close> \<open>M(X)\<close>
+    show "{y\<in>X. d(A)`y = 1} = A"
+      using Pow_rel_char by auto
+  next
+    fix f
+    assume "f: X\<rightarrow>\<^bsup>M\<^esup> 2"
+    with assms
+    have "f: X\<rightarrow> 2" "M(f)" using function_space_rel_char by simp_all
+    then
+    show "d({y \<in> X . f ` y = 1}) = f"
+      using apply_type[OF \<open>f: X\<rightarrow>2\<close>] by (force intro:fun_extension)
+    from \<open>M(X)\<close> \<open>M(f)\<close>
+    show "{ya \<in> X . f ` ya = 1} \<in> Pow\<^bsup>M\<^esup>(X)"
+      using Pow_rel_char separation_equal_apply by auto
+  next
+    from assms \<open>lam_replacement(M, \<lambda>x. d(x))\<close>
+      \<open>\<And>A. _ \<Longrightarrow> lam_replacement(M, \<lambda>x. bool_of_o(x\<in>A))\<close>
+    show "M(\<lambda>x\<in>Pow\<^bsup>M\<^esup>(X). d(x))"
+      using lam_replacement_iff_lam_closed[THEN iffD1] by auto
+  qed (auto simp:\<open>M(X)\<close>)
+qed
 
 lemma Pow_rel_bottom: "M(B) \<Longrightarrow> 0 \<in> Pow\<^bsup>M\<^esup>(B)"
   using Pow_rel_char by simp
