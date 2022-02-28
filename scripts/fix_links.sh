8bdb19eeb6fee1ea0754e849550043e2e61b822e
Pedro Sánchez Terraf <pedrost@gmail.com>
2022-02-27 21:09:28 -0300

    updating Makefiles and associated script

--- scripts/fix_links.sh
+++ scripts/fix_links.sh
@@ -3,8 +3,7 @@ old_dir=$PWD
 html_dir="$1"
 
 cd $html_dir
-sed -i -- 's/a href="..\/ZF\/ZF.html/a href="https:\/\/www.cl.cam.ac.uk\/research\/hvg\/Isabelle\/dist\/library\/ZF\/ZF\/ZF.html/g' *.html
-sed -i -- 's/a href="..\/ZF\/ZF\//a href="https:\/\/www.cl.cam.ac.uk\/research\/hvg\/Isabelle\/dist\/library\/ZF\/ZF\//g' *.html
-sed -i -- 's/a href="..\/ZF\/ZF-Constructible\//a href="https:\/\/www.cl.cam.ac.uk\/research\/hvg\/Isabelle\/dist\/library\/ZF\/ZF-Constructible\//g' *.html
-sed -i -- 's|a href="../ZF-Constructible-Trans/|a href="https://cs.famaf.unc.edu.ar/~pedro/forcing/ZF-Constructible-Trans/|g' *.html
+sed -i -- 's|a href="../ZF/|a href="https://www.cl.cam.ac.uk/research/hvg/Isabelle/dist/library/ZF/ZF/|g' *.html
+sed -i -- 's|a href="../ZF-Constructible/|a href="https://www.cl.cam.ac.uk/research/hvg/Isabelle/dist/library/ZF/ZF-Constructible/|g' *.html
+sed -i -- 's|a href="../../AFP/Delta_System_Lemma/|a href="https://www.isa-afp.org/browser_info/current/AFP/Delta_System_Lemma/|g' *.html
 cd $old_dir
