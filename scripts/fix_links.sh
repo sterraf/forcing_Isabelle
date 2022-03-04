f4e38f1c08d8b57f5e04e7d42cbd125a9f045ce9
Pedro Sánchez Terraf <pedrost@gmail.com>
2022-03-04 11:11:14 -0300

    pattern-match all AFP entries

--- scripts/fix_links.sh
+++ scripts/fix_links.sh
@@ -5,5 +5,5 @@ html_dir="$1"
 cd $html_dir
 sed -i -- 's|a href="../ZF/|a href="https://www.cl.cam.ac.uk/research/hvg/Isabelle/dist/library/ZF/ZF/|g' *.html
 sed -i -- 's|a href="../ZF-Constructible/|a href="https://www.cl.cam.ac.uk/research/hvg/Isabelle/dist/library/ZF/ZF-Constructible/|g' *.html
-sed -i -- 's|a href="../../AFP/Delta_System_Lemma/|a href="https://www.isa-afp.org/browser_info/current/AFP/Delta_System_Lemma/|g' *.html
+sed -i -- 's|a href="../../AFP/|a href="https://www.isa-afp.org/browser_info/current/AFP/|g' *.html
 cd $old_dir
