4c087e4a0af8b2a6eb864734f67a20c4de1a8ce4
Miguel Pagano <miguel.pagano@gmail.com>
2022-03-05 16:48:06 -0300

    Pairing.

--- src/Pairing_Axiom.thy
+++ src/Pairing_Axiom.thy
@@ -16,32 +16,37 @@ lemma pairing_in_MG :
   assumes "M_generic(G)"
   shows "upair_ax(##M[G])"
 proof -
+  from assms
+  have types: "\<one>\<in>G" "\<one>\<in>P" "\<one>\<in>M"
+    using one_in_G one_in_M one_in_P
+    by simp_all
   {
     fix x y
-    have "\<one>\<in>G" using assms one_in_G by simp
-    from assms
-    have "G\<subseteq>P" unfolding M_generic_def and filter_def by simp
-    with \<open>\<one>\<in>G\<close>
-    have "\<one>\<in>P" using subsetD by simp
-    then
-    have "\<one>\<in>M" using transitivity[OF _ P_in_M] by simp
+    note assms types
+    moreover
     assume "x \<in> M[G]" "y \<in> M[G]"
-    then
-    obtain \<tau> \<rho> where
-      0 : "val(P,G,\<tau>) = x" "val(P,G,\<rho>) = y" "\<rho> \<in> M"  "\<tau> \<in> M"
+    moreover from this
+    obtain \<tau> \<rho> where "val(P,G,\<tau>) = x" "val(P,G,\<rho>) = y" "\<rho> \<in> M"  "\<tau> \<in> M"
       using GenExtD by blast
-    with \<open>\<one>\<in>M\<close>
-    have "\<langle>\<tau>,\<one>\<rangle> \<in> M" "\<langle>\<rho>,\<one>\<rangle>\<in>M" using pair_in_M_iff by auto
-    then
-    have 1: "{\<langle>\<tau>,\<one>\<rangle>,\<langle>\<rho>,\<one>\<rangle>} \<in> M" (is "?\<sigma> \<in> _") using upair_in_M_iff by simp
-    then
-    have "val(P,G,?\<sigma>) \<in> M[G]" using GenExtI by simp
-    with 1
-    have "{val(P,G,\<tau>),val(P,G,\<rho>)} \<in> M[G]" using val_Upair assms one_in_G by simp
-    with 0
-    have "{x,y} \<in> M[G]" by simp
+    moreover from types this
+    have "\<langle>\<tau>,\<one>\<rangle> \<in> M" "\<langle>\<rho>,\<one>\<rangle>\<in>M"
+      using pair_in_M_iff by auto
+    moreover from this
+    have "{\<langle>\<tau>,\<one>\<rangle>,\<langle>\<rho>,\<one>\<rangle>} \<in> M" (is "?\<sigma> \<in> _")
+      using upair_in_M_iff by simp
+    moreover from this
+    have "val(P,G,?\<sigma>) \<in> M[G]"
+      using GenExtI by simp
+    moreover from calculation
+    have "{val(P,G,\<tau>),val(P,G,\<rho>)} \<in> M[G]"
+      using val_Upair assms one_in_G by simp
+    ultimately
+    have "{x,y} \<in> M[G]"
+      by simp
   }
-  then show ?thesis unfolding upair_ax_def upair_def by auto
+  then
+  show ?thesis
+    unfolding upair_ax_def upair_def by auto
 qed
 
 end \<comment> \<open>\<^locale>\<open>forcing_data1\<close>\<close>
