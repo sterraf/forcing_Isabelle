6f6b4176005600078bc95984b8779a2c2db465d6
Miguel Pagano <miguel.pagano@gmail.com>
2022-03-05 16:29:08 -0300

    Extensionality and Ordinals.

--- src/Extensionality_Axiom.thy
+++ src/Extensionality_Axiom.thy
@@ -9,24 +9,19 @@ context forcing_data1
 begin
 
 lemma extensionality_in_MG : "extensionality(##(M[G]))"
-proof -
-  {
-    fix x y z
-    assume
-      asms: "x\<in>M[G]" "y\<in>M[G]" "(\<forall>w\<in>M[G] . w \<in> x \<longleftrightarrow> w \<in> y)"
-    from \<open>x\<in>M[G]\<close> have
-      "z\<in>x \<longleftrightarrow> z\<in>M[G] \<and> z\<in>x"
-      using transitivity_MG by auto
-    also have
-      "... \<longleftrightarrow> z\<in>y"
-      using asms transitivity_MG by auto
-    finally have
-      "z\<in>x \<longleftrightarrow> z\<in>y" .
-  }
-  then have
-    "\<forall>x\<in>M[G] . \<forall>y\<in>M[G] . (\<forall>z\<in>M[G] . z \<in> x \<longleftrightarrow> z \<in> y) \<longrightarrow> x = y"
-    by blast
-  then show ?thesis unfolding extensionality_def by simp
+  unfolding extensionality_def
+proof(clarsimp)
+  fix x y
+  assume "x\<in>M[G]" "y\<in>M[G]" "(\<forall>w\<in>M[G] . w \<in> x \<longleftrightarrow> w \<in> y)"
+  moreover from this
+  have "z\<in>x \<longleftrightarrow> z\<in>M[G] \<and> z\<in>y" for z
+    using transitivity_MG by auto
+  moreover from calculation
+  have "z\<in>M[G] \<and> z\<in>x \<longleftrightarrow> z\<in>y" for z
+    using transitivity_MG by auto
+  ultimately
+  show "x=y"
+    by auto
 qed
 
 end \<comment> \<open>\<^locale>\<open>forcing_data1\<close>\<close>
