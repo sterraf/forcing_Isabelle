4d16bf1f889d520464f10850675d8d0e6ac2419c
Pedro Sánchez Terraf <pedrost@gmail.com>
2022-07-16 11:55:47 -0300

    Restricting simpsets whenever possible

--- src/Absolute_Versions.thy
+++ src/Absolute_Versions.thy
@@ -80,7 +80,7 @@ lemma cardinal_rel_absolute[V_simps]: "|x|\<^bsup>\<V>\<^esup> = |x|"
   unfolding cardinal_def cardinal_rel_def by (simp add:V_simps)
 
 lemma Card_rel_absolute[V_simps]:"Card\<^bsup>\<V>\<^esup>(a) \<longleftrightarrow> Card(a)"
-  unfolding Card_rel_def Card_def by (simp add:V_simps)
+  unfolding Card_rel_def Card_def by (simp only:V_simps)
 
 lemma csucc_rel_absolute[V_simps]:"(a\<^sup>+)\<^bsup>\<V>\<^esup> = a\<^sup>+"
   unfolding csucc_rel_def csucc_def by (simp add:V_simps)
@@ -89,7 +89,7 @@ lemma function_space_rel_absolute[V_simps]:"x \<rightarrow>\<^bsup>\<V>\<^esup> 
   using V.function_space_rel_char by (simp add:V_simps)
 
 lemma cexp_rel_absolute[V_simps]:"x\<^bsup>\<up>y,\<V>\<^esup> = x\<^bsup>\<up>y\<^esup>"
-  unfolding cexp_rel_def cexp_def by (simp add:V_simps)
+  unfolding cexp_rel_def cexp_def by (simp only:V_simps)
 
 lemma HAleph_rel_absolute[V_simps]:"HAleph_rel(\<V>,a,b) = HAleph(a,b)"
   unfolding HAleph_rel_def HAleph_def by (auto simp add:V_simps)
@@ -101,7 +101,7 @@ proof -
     unfolding Aleph_rel_def by simp
   also
   have "\<dots> = transrec(x, HAleph)"
-    by (simp add:V_simps)
+    by (simp only:V_simps)
   also from \<open>Ord(x)\<close>
   have "\<dots> = \<aleph>\<^bsub>x\<^esub>"
     using Aleph'_eq_Aleph unfolding Aleph'_def by simp
@@ -121,11 +121,11 @@ text\<open>These two results are new, first obtained in relative form
     (not ported).\<close>
 lemma csucc_cardinal:
   assumes "Ord(\<kappa>)" shows "|\<kappa>|\<^sup>+ = \<kappa>\<^sup>+"
-  using assms V.csucc_rel_cardinal_rel by (simp add:V_simps)
+  using assms V.csucc_rel_cardinal_rel by (simp only:V_simps)
 
 lemma csucc_le_mono:
   assumes "\<kappa> \<le> \<nu>"  shows "\<kappa>\<^sup>+ \<le> \<nu>\<^sup>+"
-  using assms V.csucc_rel_le_mono by (simp add:V_simps)
+  using assms V.csucc_rel_le_mono by (simp only:V_simps)
 
 text\<open>Example of transferring results from a transitive model to \<^term>\<open>\<V>\<close>\<close>
 lemma (in M_Perm) eqpoll_rel_transfer_absolute:
